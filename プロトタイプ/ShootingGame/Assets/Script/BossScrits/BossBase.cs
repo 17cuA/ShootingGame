4054376ed7d380fe70f476d073f0bc80487b690a
wanarisu <wanarisu@gmail.com>
2019-04-26 08:16:51 +0900

    Bossテンプレ（仮）

---
+++ プロトタイプ/ShootingGame/Assets/Script/BossScrits/BossBase.cs
@@ -0,0 +1,82 @@
+﻿//----------------------------------------------------------------------------------------------
+// 制作者：諸岡勇樹
+// 制作日：2019/04/25
+//----------------------------------------------------------------------------------------------
+// Bossのベースになる挙動
+//----------------------------------------------------------------------------------------------
+// 2019/04/25：体パーツの格納、各パーツの生存確認
+//----------------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class BossBase : MonoBehaviour
+{
+	/// <summary>
+	/// 自分のパーツ（子供）
+	/// </summary>
+	private List<GameObject> ownParts { set; get; }
+
+	void Start()
+    {
+		ownParts = new List<GameObject>();
+		for(int i = 0; i < transform.childCount; i++)
+		{
+			ownParts.Add(transform.GetChild(i).gameObject);
+		}
+	}
+
+    void Update()
+    {
+		PartFactorDeletion();
+		
+		// パーツがなくなったとき
+		if(Is_PartsAlive())
+		{
+			OwnDeletion();
+		}
+	}
+
+	/// <summary>
+	/// パーツのリストの中が null のとき要素の削除
+	/// </summary>
+	private void PartFactorDeletion()
+	{
+		// 各パーツの確認
+		for (int i = 0; i < ownParts.Count; i++)
+		{
+			// null のとき
+			if (ownParts[i] == null)
+			{
+				// 要素削除
+				ownParts.RemoveAt(i);
+			}
+		}
+	}
+
+	/// <summary>
+	/// パーツの生存確認
+	/// </summary>
+	/// <returns>パーツがないとき true</returns>
+	private bool Is_PartsAlive()
+	{
+		if(ownParts.Count == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/// <summary>
+	/// 疑似デストラクタ
+	/// </summary>
+	private void OwnDeletion()
+	{
+
+		Destroy(gameObject);
+	}
+}
