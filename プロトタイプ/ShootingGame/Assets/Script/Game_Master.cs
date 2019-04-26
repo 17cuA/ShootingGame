590d62c6e60388ebb5fedbbe444f6989e9aed24c
741PC-14\student <cu.common@gmail.com>
2019-04-26 10:31:13 +0900

    ゲームの流れを管理するスクリプト（Game_Master）を作成

---
+++ プロトタイプ/ShootingGame/Assets/Script/Game_Master.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Game_Master : MonoBehaviour
+{
+    private uint frame_Count;   //  ゲームが開始してからの時間をカウント
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frame_Count++;
+    }
+}
