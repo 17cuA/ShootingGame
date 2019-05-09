521b8aab7d0dbc0857b7f3a585db3b94712379e5
tatsuotsu <tatsuyij@gmail.com>
2019-05-10 01:24:05 +0900

    敵が回転しながら来る処理、途中

--- プロトタイプ/ShootingGame/Assets/Script/EnemyScript/Screw_Enemy.cs
+++ プロトタイプ/ShootingGame/Assets/Script/EnemyScript/Screw_Enemy.cs
@@ -9,15 +9,19 @@ using UnityEngine;
 
 public class Screw_Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float speed;
+	void Update()
+	{
+		//float f = 1.0f / speed;
+		//float sin = Mathf.Sin(2 * Mathf.PI * f * Time.time) / 1;
+		//transform.position = new Vector3(0, sin, 0);
+		Vector3 pos = Vector3.zero; //中心を決めます。今回は(0,0,0)
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		//範囲を指定してあげると大きな円、小さな円を実装できます。
+		//pos.y += Mathf.Sin(Time.time * speed) * 2f;
+		pos.x += Mathf.Sin(Time.time * speed) * 2f;
+		pos.y += Mathf.Cos(Time.time * speed) * 2f;
+		pos.z += 0.1f;
+		transform.position += pos;
+	}
 }
