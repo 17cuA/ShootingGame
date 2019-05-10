beda7fd60ee3c4df41e705d93cfb87c14d21146e
741PC-32\student <cu.common@gmail.com>
2019-05-10 14:58:46 +0900

    スクリューする敵キャラ作成完了

--- プロトタイプ/ShootingGame/Assets/Script/EnemyScript/Screw_Enemy.cs
+++ プロトタイプ/ShootingGame/Assets/Script/EnemyScript/Screw_Enemy.cs
@@ -10,27 +10,18 @@ using UnityEngine;
 public class Screw_Enemy : MonoBehaviour
 {
 	public float X_speed;
-	Vector3 startpos;
-
-	private void Start()
+	Vector3 pos;
+	Vector3 spos;
+		private void Start()
 	{
-		startpos = transform.position;
+		spos = transform.position;
 	}
 	void Update()
 	{
-		//float f = 1.0f / speed;
-		//float sin = Mathf.Sin(2 * Mathf.PI * f * Time.time) / 1;
-		//transform.position = new Vector3(0, sin, 0);
-		Vector3 pos = Vector3.zero; //中心を決めます。今回は(0,0,0)
-
-		//範囲を指定してあげると大きな円、小さな円を実装できます。
-		//pos.y += Mathf.Sin(Time.time * speed) * 2f;
-		pos.z += Mathf.Sin(Time.time * X_speed) * 2f;
-		pos.y += Mathf.Cos(Time.time * X_speed) * 2f;
+		pos = Vector3.zero; //中心を決めます。今回は(0,0,0)
+		//pos.z += Mathf.Sin(Time.time * X_speed + transform.localPosition.x) * 2f;
+		pos.y += Mathf.Cos(Time.time * X_speed + transform.localPosition.x) * 2f;
 		pos.x +=  transform.position.x + 1 * Time.deltaTime;
-		//pos.z += 0.1f;
 		transform.position = pos;
-		//transform.localPosition = startpos + pos; //目的地に移動
-		//speed-= 0.01f;
 	}
 }
