9878d8c55bfbb9b46ede390e7952e412bca0a332
741PC-17\student <cu.common@gmail.com>
2019-05-10 19:21:45 +0900

    Enemy見た目変更

--- プロトタイプ/ShootingGame/Assets/Script/morooka/Game_Master.cs
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/Game_Master.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class Game_Master : MonoBehaviour
 {
     public uint Frame_Count { private set; get; }   //  ゲームが開始してからの時間をカウント
     public static Game_Master MY { get; private set; }
+    public static int Score{set;get;}       // スコア
 
     void Start()
     {
