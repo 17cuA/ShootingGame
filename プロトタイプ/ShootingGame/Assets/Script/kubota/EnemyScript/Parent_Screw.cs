96bf99bd6bedf1a98e95bbefd76dc5d6a5674dec
tatsuotsu <tatsuyij@gmail.com>
2019-05-13 02:10:13 +0900

    スクリューの敵作成途中

---
+++ プロトタイプ/ShootingGame/Assets/Script/kubota/EnemyScript/Parent_Screw.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parent_Screw : MonoBehaviour
+{
+	public float moving_distance;	//円運動の直径の動きを入れる変数
+}
--- プロトタイプ/ShootingGame/Assets/Script/kubota/EnemyScript/Screw_Enemy.cs
+++ プロトタイプ/ShootingGame/Assets/Script/kubota/EnemyScript/Screw_Enemy.cs
@@ -9,19 +9,24 @@ using UnityEngine;
 
 public class Screw_Enemy : MonoBehaviour
 {
-	public float X_speed;
-	Vector3 pos;
-	Vector3 spos;
-		private void Start()
+	public float X_speed;		//Ｘ軸の行動の時のスピード
+	public float Y_speed;		//Ｙ軸の行動の時のスピード
+	public float Z_speed;       //Ｚ軸の行動の時のスピード
+
+	private GameObject _parent;
+	private Parent_Screw PS;
+
+	Vector3 pos;				//複雑な動きをするときに計算結果をxyzごとに入れまとめて動かす
+	private void Start()
 	{
-		spos = transform.position;
+		
 	}
 	void Update()
 	{
 		pos = Vector3.zero; //中心を決めます。今回は(0,0,0)
-		//pos.z += Mathf.Sin(Time.time * X_speed + transform.localPosition.x) * 2f;
-		pos.y += Mathf.Cos(Time.time * X_speed + transform.localPosition.x) * 2f;
-		pos.x +=  transform.position.x + 1 * Time.deltaTime;
+		pos.z += Mathf.Sin(Time.time * X_speed + transform.localPosition.x) * 2f;
+		pos.y += Mathf.Cos(Time.time * X_speed + transform.localPosition.x) *6f;
+		pos.x +=  transform.position.x + 1 * Time.deltaTime ;
 		transform.position = pos;
 	}
 }
