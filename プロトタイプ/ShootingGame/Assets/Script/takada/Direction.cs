36c116e1d7457ea2d93bc7f8679463e74e13d1ce
741PC-22\student <cu.common@gmail.com>
2019-05-13 13:57:12 +0900

    爆発エフェクト追加

---
+++ プロトタイプ/ShootingGame/Assets/Script/takada/Direction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Direction : MonoBehaviour
+{
+	public ParticleManagement particleManagementCS;
+	public Vector3 destination;
+	public Vector3 mousePostion;
+	void Start()
+	{
+		particleManagementCS = GameObject.Find("ParticleManager").GetComponent<ParticleManagement>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//マウス追従
+		mousePostion = Input.mousePosition;
+		mousePostion.z = 10f;
+		destination = Camera.main.ScreenToWorldPoint(mousePostion);
+		transform.position = destination;
+
+		//爆発パターン
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			particleManagementCS.ParticleCreation(0, transform.position);
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			particleManagementCS.ParticleCreation(1, transform.position);
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			particleManagementCS.ParticleCreation(2, transform.position);
+		}
+	}
+}
---
+++ プロトタイプ/ShootingGame/Assets/Script/takada/ParticleManagement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManagement : MonoBehaviour
+{
+	/*
+	 public ParticleManagement particleManagementCS;
+	 particleManagementCS = GameObject.Find("ParticleManager").GetComponent<ParticleManagement>();
+	 particleManagementCS.ParticleCreation(0,transform.position);
+	 */
+	[SerializeField]
+	public GameObject[] particle = new GameObject[3];
+
+	//void Start(){}
+
+	void Update()
+	{
+	}
+
+	public void ParticleCreation(int particleID,Vector3 objectPosition)
+	{
+		Instantiate(particle[particleID], objectPosition, transform.rotation);
+	}
+}
