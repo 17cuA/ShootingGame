170399236dae64678ef382b641efe0a02a4dffa3
741PC-19\student <cu.common@gmail.com>
2019-05-15 14:53:08 +0900

    往復する敵途中（ｘ座標とシンクロした編隊行動ができていない）

---
+++ プロトタイプ/ShootingGame/Assets/Script/kubota/EnemyScript/Round_Trip_Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Round_Trip_Enemy : MonoBehaviour
+{
+	public float X_speed;		//Ｘ軸の行動の時のスピード
+	public float Y_speed;		//Ｙ軸の行動の時のスピード
+	public float Z_speed;       //Ｚ軸の行動の時のスピード
+	public int a;
+	Vector3 pos;				//複雑な動きをするときに計算結果をxyzごとに入れまとめて動かす
+	private float start_pos_x;
+    void Start()
+    {
+		start_pos_x = transform.position.x;
+		pos = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//pos = Vector3.zero;
+		pos = new Vector3(start_pos_x,0,0); //中心を決めます。今回は(0,0,0)
+		float x1 = (Mathf.Pow(Time.time - a, 3) / 2) * X_speed;
+		float x2 = Mathf.Pow(Time.time, 2) * Y_speed;
+		float x3 = Time.time * Z_speed;
+		pos.x += x1 - x2 ;
+		transform.position = -pos;
+    }
+}
--- プロトタイプ/ShootingGame/Assets/Script/kubota/EnemyScript/Screw_Enemy.cs
+++ プロトタイプ/ShootingGame/Assets/Script/kubota/EnemyScript/Screw_Enemy.cs
@@ -12,7 +12,7 @@ public class Screw_Enemy : MonoBehaviour
 	public float X_speed;		//Ｘ軸の行動の時のスピード
 	public float Y_speed;		//Ｙ軸の行動の時のスピード
 	public float Z_speed;       //Ｚ軸の行動の時のスピード
-
+	public int a;
 	private GameObject _parent;
 	private Parent_Screw PS;
 	private float distance_Z;
@@ -28,27 +28,31 @@ public class Screw_Enemy : MonoBehaviour
 		distance_Z = PS.moving_distance_Z;
 		distance_Y = PS.moving_distance_Y;
 		start_pos_y = transform.position.y;
+		pos = Vector3.zero;
 	}
 	void Update()
 	{
-		pos = new Vector3(0,0,0); //中心を決めます。今回は(0,0,0)
-		//pos.z += Mathf.Sin(Time.time * X_speed + transform.localPosition.x) * distance_Z;
-		//pos.y += Mathf.Cos(Time.time * X_speed + transform.localPosition.x) *distance_Y;
-		//pos.x +=  transform.position.x + 1 * Time.deltaTime  * -1;
-		//pos.y = (1-Mathf.Cos(Mathf.PI * Time.time)) /2;
-		//pos.y = (((int)(Time.time)^3)/3)-Y_speed*((int)(Time.time)^2)+((3*Y_speed - 2)*Time.time);
-		//pos.z +=  transform.position.z + 1 * Time.deltaTime * -1;
-		//float a = Time.time;
-		//for (int i = 0; i < 3; i++) a *= Time.time;
-		//float b = -(a + Z_speed) / 3.0f;
-		//float c = -Y_speed * Time.time;
-		//pos.x = b - c;
-		float x1 = Mathf.Pow(Time.time - 1,3) * X_speed;
-		float x2 = Mathf.Pow(Time.time,2) * Y_speed;
-		float x3 = Time.time * Z_speed;
-		pos.x = x1 + x2 + x3;
-		transform.position = pos;
-		Debug.Log(power_3());
+		pos = new Vector3(0,start_pos_y,0); //中心を決めます。今回は(0,0,0)
+        pos.z += Mathf.Sin(Time.time * X_speed + transform.localPosition.x) * distance_Z;
+        pos.y += Mathf.Cos(Time.time * X_speed + transform.localPosition.x) * distance_Y;
+        pos.x += transform.position.x + 1 * Time.deltaTime * -1;
+        //pos.y = (1-Mathf.Cos(Mathf.PI * Time.time)) /2;
+        //pos.y = (((int)(Time.time)^3)/3)-Y_speed*((int)(Time.time)^2)+((3*Y_speed - 2)*Time.time);
+        //pos.z +=  transform.position.z + 1 * Time.deltaTime * -1;
+        //float a = Time.time;
+        //for (int i = 0; i < 3; i++) a *= Time.time;
+        //float b = -(a + Z_speed) / 3.0f;
+        //float c = -Y_speed * Time.time;
+        //pos.x = b - c;
+
+        //----------------------------------------
+        //float x1 = (Mathf.Pow(Time.time - a,3)/2) * X_speed;
+        //float x2 = Mathf.Pow(Time.time, 2) * Y_speed;
+        //float x3 = Time.time * Z_speed;
+        //pos.x += x1 - x2 + x3 ;
+        //-----------------------------------------
+        transform.position = pos;
+		//Debug.Log(power_3());
 	}
 
 	float power_3()
