6a79cba1af48b7618a714d0b779112b6111f85bf
741PC-23\student <cu.common@gmail.com>
2019-05-15 19:18:19 +0900

    敵キャラの移動（ひょっこりはんはできているが、速度がどんどんあがっていってしまう。） また、反転の挙動を上げていかなければいけない。

--- プロトタイプ/ShootingGame/Assets/Script/kubota/EnemyScript/Round_Trip_Enemy.cs
+++ プロトタイプ/ShootingGame/Assets/Script/kubota/EnemyScript/Round_Trip_Enemy.cs
@@ -14,6 +14,10 @@ public class Round_Trip_Enemy : MonoBehaviour
     {
 		start_pos_x = transform.position.x;
 		pos = Vector3.zero;
+        X_speed = 1.5f;
+        Y_speed = 4f;
+        Z_speed = -2;
+        a = 5;
     }
 
     // Update is called once per frame
@@ -21,10 +25,10 @@ public class Round_Trip_Enemy : MonoBehaviour
     {
 		//pos = Vector3.zero;
 		pos = new Vector3(start_pos_x,0,0); //中心を決めます。今回は(0,0,0)
-		float x1 = (Mathf.Pow(Time.time - a, 3) / 2) * X_speed;
-		float x2 = Mathf.Pow(Time.time, 2) * Y_speed;
-		float x3 = Time.time * Z_speed;
-		pos.x += x1 - x2 ;
-		transform.position = -pos;
+		float x1 = (Mathf.Pow(Time.time - a, 3) * X_speed) / 1;
+		float x2 = Mathf.Pow(Time.time - a, 2) * Y_speed;
+		float x3 = (Time.time - a) * Z_speed;
+		pos.x += x1 - x2 + x3;
+		transform.position = pos;
     }
 }
--- プロトタイプ/ShootingGame/Assets/Script/kubota/EnemyScript/Screw_Enemy.cs
+++ プロトタイプ/ShootingGame/Assets/Script/kubota/EnemyScript/Screw_Enemy.cs
@@ -33,26 +33,10 @@ public class Screw_Enemy : MonoBehaviour
 	void Update()
 	{
 		pos = new Vector3(0,start_pos_y,0); //中心を決めます。今回は(0,0,0)
-        pos.z += Mathf.Sin(Time.time * X_speed + transform.localPosition.x) * distance_Z;
+        pos.z += Mathf.Sin(Time.time * X_speed + transform.localPosition.x ) * distance_Z;
         pos.y += Mathf.Cos(Time.time * X_speed + transform.localPosition.x) * distance_Y;
         pos.x += transform.position.x + 1 * Time.deltaTime * -1;
-        //pos.y = (1-Mathf.Cos(Mathf.PI * Time.time)) /2;
-        //pos.y = (((int)(Time.time)^3)/3)-Y_speed*((int)(Time.time)^2)+((3*Y_speed - 2)*Time.time);
-        //pos.z +=  transform.position.z + 1 * Time.deltaTime * -1;
-        //float a = Time.time;
-        //for (int i = 0; i < 3; i++) a *= Time.time;
-        //float b = -(a + Z_speed) / 3.0f;
-        //float c = -Y_speed * Time.time;
-        //pos.x = b - c;
-
-        //----------------------------------------
-        //float x1 = (Mathf.Pow(Time.time - a,3)/2) * X_speed;
-        //float x2 = Mathf.Pow(Time.time, 2) * Y_speed;
-        //float x3 = Time.time * Z_speed;
-        //pos.x += x1 - x2 + x3 ;
-        //-----------------------------------------
         transform.position = pos;
-		//Debug.Log(power_3());
 	}
 
 	float power_3()
