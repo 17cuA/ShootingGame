cccce19ea7d67f9e49b92fe8ea2f383779d39289
741PC-17\student <cu.common@gmail.com>
2019-05-17 16:46:26 +0900

    スコア表示＿1

--- プロトタイプ/ShootingGame/Assets/Script/morooka/Game_Master.cs
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/Game_Master.cs
@@ -12,44 +12,84 @@ using CSV_Management;
 
 public class Game_Master : MonoBehaviour
 {
-	/// <summary>
-	/// ボスのデータベースにあるレコード管理
-	/// </summary>
-	public　enum BOSS_DATA_ELEMENTS
-	{
-		eID,					// BossのID
-		eNAME,					// Boss の名前
-		eSCRIPT,				// 各 Boss 個別のスクリプト
-		eATTACK_INTERVAL,		// 攻撃と攻撃のインターバル
-		eACT_CHANGE,			// 攻撃種類の切り替えインターバル
-		ePARTS_HP,				// Boos のパーツのHP
-		eSCORE,					// Boss の持つHP
-		eBULLET_NAME_1,			// 攻撃パターン1
-		eBULLET_NAME_2,			// 攻撃パターン2
-		eBULLET_NAME_3,			// 攻撃パターン3
-		eBULLET_NAME_4,			// 攻撃パターン4
-		eBULLET_NAME_5,			// 攻撃パターン5
-		eEFFECT					// エフェクト
-	}
+    /// <summary>
+    /// ボスのデータベースにあるレコード管理
+    /// </summary>
+    public enum BOSS_DATA_ELEMENTS
+    {
+        eID,                    // BossのID
+        eNAME,                  // Boss の名前
+        eSCRIPT,                // 各 Boss 個別のスクリプト
+        eATTACK_INTERVAL,       // 攻撃と攻撃のインターバル
+        eACT_CHANGE,            // 攻撃種類の切り替えインターバル
+        ePARTS_HP,              // Boos のパーツのHP
+        eSCORE,                 // Boss の持つHP
+        eBULLET_NAME_1,         // 攻撃パターン1
+        eBULLET_NAME_2,         // 攻撃パターン2
+        eBULLET_NAME_3,         // 攻撃パターン3
+        eBULLET_NAME_4,         // 攻撃パターン4
+        eBULLET_NAME_5,         // 攻撃パターン5
+        eEFFECT                 // エフェクト
+    }
 
-	public uint Frame_Count { private set; get; }				// ゲームが開始してからの時間をカウント
-    public static Game_Master MY { get; private set; }			// 自分の情報
-    public static int Score{ set; get; }						// スコア
-	public Database_Manager Boss_Data { private set; get; }		// ボスのデータベース
+    /// <summary>
+    /// エネミーのデータベースにあるレコード管理
+    /// </summary>
+    public enum ENEMY_DATA_ELEMENTS
+    {
+        eID,                    // Enemy のID
+        eNAME,                  // Enemy の名前
+        eHP,                    // HP
+        eATTACK_INTERVAL,		// 攻撃と攻撃のインターバル
+        eBULLET,                // 弾
+        eEFFECT					// エフェクト
+    }
+
+    public uint Frame_Count{private set; get;}                  // ゲームが開始してからの時間をカウント
+    public static Game_Master MY{get; private set;}             // 自分の情報
+    public uint display_score{private set; get;}                // 表示スコア
+    public byte[] accumulate_score{private set; get;}           // 溜めスコア
+    public Database_Manager Boss_Data{private set; get;}        // ボスのデータベース
+    public Database_Manager Enemy_Data{private set; get;}       // エネミーのデータベース
 
     void Start()
     {
-		if(Boss_Data == null)
-		{
-			Boss_Data = new Database_Manager();
-			Boss_Data.CSVArrangement("Boss/Boss_Data");
-		}
-		MY = GetComponent<Game_Master>();
+        accumulate_score = new byte[10];
+
+        if (Boss_Data == null)
+        {
+            Boss_Data = new Database_Manager();
+            Boss_Data.CSVArrangement("Boss/Boss_Data");
+        }
+        MY = GetComponent<Game_Master>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Frame_Count++;
+    }
+
+    /// <summary>
+    /// 取得スコアの加算
+    /// </summary>
+    /// <param name="addition"> 加算数 </param>
+    public void Score_Addition(uint addition)
+    {
+        uint confirmation_digits = 10;      // 確認桁数
+
+        // 各桁確認の繰り返し
+        for (byte b = 0; b < accumulate_score.Length; b++)
+        {
+            accumulate_score[b] += (byte)(addition % confirmation_digits);
+            addition/=confirmation_digits;
+            if (accumulate_score[b] > 10)
+            {
+                accumulate_score[b] -= 10;
+                if (b != accumulate_score.Length - 1)
+                {
+                    accumulate_score[b + 1] += 1;
+                }
+            }
+        }
     }
 }
---
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/ScoreDis.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreDis : MonoBehaviour
+{ 
+    private Sprite[] sprites;                            // スプライトの保存
+    private SpriteRenderer[] renderers;                  // レンダー情報の保存
+    private GameObject[] digit_numbers;                  // 各桁の数の変数
+
+    public int digits{ private set; get;}       // 桁数
+    public byte[] Display_Score{private set; get;}      // スコア
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Display_Score = new byte[digits];
+        renderers = new SpriteRenderer[digits];
+        digit_numbers = new GameObject[digits];
+
+        for(byte i = 0; i < digits;i++)
+        {
+            
+            renderers[i] = transform.GetChild(i).GetComponent<SpriteRenderer>();
+            renderers[i].sprite = sprites[0];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var jjjj = 1;
+        Score_Addition((uint)jjjj);
+
+        for(byte i = 0;i < renderers.Length; i++)
+        {
+            renderers[i].sprite =  sprites[Display_Score[i]];
+        }
+    }
+
+    /// <summary>
+    /// 取得スコアの加算
+    /// </summary>
+    /// <param name="addition"> 加算数 </param>
+    public void Score_Addition(uint addition)
+    {
+        uint confirmation_digits = 10;      // 確認桁数
+
+        // 各桁確認の繰り返し
+        for (byte b = 0; b < Display_Score.Length; b++)
+        {
+            Display_Score[b] += (byte)(addition % confirmation_digits);
+            addition /= confirmation_digits;
+            if (Display_Score[b] >= 10)
+            {
+                Display_Score[b] -= 10;
+                if (b != Display_Score.Length - 1)
+                {
+                    Display_Score[b + 1] += 1;
+                }
+            }
+        }
+    }
+
+}
