b355346964c46653191f7892e6e7535e5a1555ff
741PC-16\student <cu.common@gmail.com>
2019-05-27 15:08:34 +0900

    ビームをline仕様に変更しました。

--- プロトタイプ/ShootingGame/Assets/Script/Sato/Line_Beam.cs
+++ プロトタイプ/ShootingGame/Assets/Script/Sato/Line_Beam.cs
@@ -1,18 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
-public class Line_Beam : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Line_Beam : MonoBehaviour {
+
+    float timer = 0.0f;
+    float effectDisplayTime = 0.2f;
+    float range = 100.0f;
+    Ray shotRay;
+    RaycastHit shotHit;  
+    ParticleSystem beamParticle;
+    LineRenderer lineRenderer;
+    private Renderer Target_Renderer;
+    // Use this for initialization
+    void Awake () {
+        beamParticle = GetComponent<ParticleSystem> ();
+        lineRenderer = GetComponent<LineRenderer> ();
+        Target_Renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+        timer += Time.deltaTime;
+
+        if (Input.GetMouseButton (1)) {
+            shot ();
+        }
+        if (timer >= effectDisplayTime)
+        {
+            disableEffect();
+        }
+  //      if (Target_Renderer.isVisible)
+		//{
+		//	Destroy(gameObject);
+		//}
+
     }
+
+    private void shot(){
+        timer = 0f;
+        beamParticle.Stop ();
+        beamParticle.Play ();
+        lineRenderer.enabled = true;
+        lineRenderer.SetPosition (0, transform.position);
+        shotRay.origin = transform.position;
+        shotRay.direction = transform.forward;
+
+        //int layerMask = 0;
+        int layerMask = LayerMask.GetMask("Enemy");
+        if(Physics.Raycast(shotRay , out shotHit , range , layerMask))
+        {
+             Destroy(shotHit.transform.gameObject);
+        }
+        lineRenderer.SetPosition(1 , shotRay.origin + shotRay.direction * range);
+
+
+    }
+
+    private void disableEffect()
+    {
+        beamParticle.Stop();
+        lineRenderer.enabled = false;
+    }
+
+
+
 }
