7f1f33d90ee9820eaef23ab7d17a3a5984f2f95e
741PC-38\student <cu.common@gmail.com>
2019-05-29 15:00:48 +0900

    totyuu

--- プロトタイプ/ShootingGame/Assets/Script/morooka/Display/Warning_Ui_Display.cs
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/Display/Warning_Ui_Display.cs
@@ -6,6 +6,7 @@
 //----------------------------------------------------------------------------------------------
 // 2019/05/24：カットイン中の挙動
 // 2019/05/27：Unity でフェード回数を決められるようにする
+// 2019/05/29：レターボックスの動きの制御
 //----------------------------------------------------------------------------------------------
 using UnityEngine;
 using TextDisplay;
@@ -30,11 +31,23 @@ public class Warning_Ui_Display : MonoBehaviour
     [SerializeField]
     [Header("フォントの位置")]
     private Vector3 position;       // フォント位置
-    //---------------------------------------------------------------------------
+    [SerializeField]
+    [Header("レターボックスのサイズ")]
+    private Vector3 letter_size;       // レターボックスのサイズ
+	[SerializeField]
+	[Header("レターボックスの移動スピード")]
+	private float letter_speed;		// レターボックスの移動スピード
+	[SerializeField]
+	[Header("カットイン時のレターボックスの位置(上の数値のみ、下は上に自動的に合わせる)")]
+	private float letter_cut_in_pos;        // カットイン時のレターボックスの位置
+	//---------------------------------------------------------------------------
 
-    private int fade_count{set;get;}         // フェード処理時のカウンター
-    public Character_Display Display {private set;get;}     // 表示する文字
-    private bool Additional_Permit {set;get;}                          // 加算許可
+	private int fade_count{set;get;}						// フェード処理時のカウンター
+    public Character_Display Display {private set;get;}		// 表示する文字
+    private bool Additional_Permit {set;get;}				// 加算許可
+	private GameObject[] Letter_Box{set; get;}				// レターボックス
+	private Vector3[] Default_Position { set;get;}			// デフォルトの位置
+	private Vector3[] Cut_In_Position {set; get;}			// カットイン時の位置
 
     private void Start()
     {
@@ -49,6 +62,16 @@ public class Warning_Ui_Display : MonoBehaviour
         Font_Color.a = 0.0f;
         Display.Set_Enable(false);
         Additional_Permit = true;
+		Letter_Box = new GameObject[2];
+
+		for(int i = 0; i < Letter_Box.Length; i ++)
+		{
+			Letter_Box[i] = transform.GetChild(i).gameObject;
+			Default_Position[i] = Letter_Box[i].transform.position;
+		}
+
+		Cut_In_Position[0].y = letter_cut_in_pos;
+		Cut_In_Position[1].y = letter_cut_in_pos * -1;
     }
 
     private void Update()
@@ -62,29 +85,6 @@ public class Warning_Ui_Display : MonoBehaviour
                 }
                 break;
             case Game_Master.CONFIGURATION_IN_STAGE.eBOSS_CUT_IN:
-                //if (fade_count == 12)
-                //{
-                //}
-                //else if (fade_count % 4 == 0)
-                //{
-                //    Font_Color.a -= fade_speed;
-                //    Display.Color_Change(Font_Color);
-
-                //    if (Font_Color.a <= 0.3f)
-                //    {
-                //        fade_count += 2;
-                //    }
-                //}
-                //else if (fade_count % 2 == 0)
-                //{
-                //    Font_Color.a += fade_speed;
-                //    Display.Color_Change(Font_Color);
-
-                //    if (Font_Color.a >= 1.0f)
-                //    {
-                //        fade_count += 2;
-                //    }
-                //}
                 if(fade_count == 0)
                 {
                     Display.Set_Enable(true);
@@ -130,6 +130,10 @@ public class Warning_Ui_Display : MonoBehaviour
             default:
                 break;
         }
-
     }
+
+	private void letter_moove()
+	{
+		
+	}
 }
