d9f60ec6289f474e473809359549fb1175225720
741PC-14\student <cu.common@gmail.com>
2019-06-07 11:50:29 +0900

    パワー統合用

--- プロトタイプ/ShootingGame/Assets/Script/Chenken/Power/Power_BulletUpgrade.cs
+++ プロトタイプ/ShootingGame/Assets/Script/Chenken/Power/Power_BulletUpgrade.cs
@@ -4,7 +4,36 @@ using Power;
 
 public class Power_BulletUpgrade : AbstractPower
 {
+	private int level = 0;
+	public int Level
+	{
+		get
+		{
+			return level;
+		}
+	}
+	private int exp = 0;
+	public int Exp
+	{
+		get
+		{
+			return exp;
+		}
+	}
+	private int getTime = 0;
+	public int GetTime
+	{
+		get
+		{
+			return getTime;
+		}
+	}
 
+	/// <summary>
+	/// 消える条件
+	/// 多分ステージクリア
+	/// ※※※※変更必要※※※※
+	/// </summary>
 	public override bool IsLost
 	{
 		get
@@ -12,29 +41,36 @@ public class Power_BulletUpgrade : AbstractPower
 			return false;
 		}
 	}
-	public Power_BulletUpgrade(PowerType type) : base(type)
+	public Power_BulletUpgrade(PowerType type, int levelUpNeedExp) : base(type, levelUpNeedExp)
 	{
-
+		this.level = 1;
 	}
 
 	public override void OnPick()
 	{
-		base.OnPick();
+		this.exp++;		//取得時経験値アップ
+		this.getTime++;//取得回数アップ
 	}
 
 	public override void OnUpdate(float deltaTime)
 	{
-		base.OnUpdate(deltaTime);
+		//現在経験値はレベルアップ必要経験値以上になると
+		if(exp >= base.value)
+		{
+			level++;	//レベルアップ
+			exp = 0;	//経験値リセット
+		}
 	}
 
 	public override void OnPickAgain()
 	{
-		base.OnPickAgain();
+		this.exp++;		 //取得時経験値アップ
+		this.getTime++; //取得回数アップ
 	}
 
 	public override void OnLost()
 	{
-		base.OnLost();
+		
 	}
 }
 
