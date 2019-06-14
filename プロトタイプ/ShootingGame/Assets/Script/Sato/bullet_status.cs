3270f70a5a0491a596a60255fad8e5d0e6f97eb2
741PC-17\student <cu.common@gmail.com>
2019-06-14 09:33:49 +0900

    totyuu

--- プロトタイプ/ShootingGame/Assets/Script/Sato/bullet_status.cs
+++ プロトタイプ/ShootingGame/Assets/Script/Sato/bullet_status.cs
@@ -3,58 +3,104 @@
 //作成者:佐藤翼
 /*
  * 2019/06/06	バレットの挙動をオブジェクトプーリングの形式に変更しました
+ * 2019/06/13	クラスをネームスペースに完全移動
  */
 using UnityEngine;
 
-public class bullet_status : MonoBehaviour
+namespace ObjectStatus
 {
-	//public enum Bullet_Type
-	//{
-	//	Single,
-	//	Double,
-	//	None
-	//}
-	//public Bullet_Type Type;
-	public float shot_speed;//弾の速度
-	public float attack_damage;//ダメージの変数
-	private Renderer Bullet_Renderer; // 判定したいオブジェクトのrendererへの参照
-	void Start()
-	{
-		Bullet_Renderer = GetComponent<Renderer>();
-		//gameObject.SetActive(false);
-	}
 
-	void Update()
+	/// <summary>
+	/// バレットに持っていてほしい基本的な処理クラス
+	/// </summary>
+	public class bullet_status : MonoBehaviour
 	{
-		transform.position = transform.position +( transform.right.normalized * shot_speed);
-
-		if (!Bullet_Renderer.isVisible)
+		//public enum Bullet_Type
+		//{
+		//	Single,
+		//	Double,
+		//	None
+		//}
+		//public Bullet_Type Type;
+		public float shot_speed;//弾の速度
+		public float attack_damage;//ダメージの変数
+		public Vector3 Travelling_Direction;    //自分の向き
+		//private Renderer Bullet_Renderer; // 判定したいオブジェクトのrendererへの参照
+		void Start()
 		{
-			gameObject.SetActive(false);
+			//Bullet_Renderer = GetComponent<Renderer>();
+			Travelling_Direction = transform.right;
 		}
-	}
-	private void OnTriggerEnter(Collider col)
-	{
-		//それぞれのキャラクタの弾が敵とプレイヤーにあたっても消えないようにするための処理
-		if (gameObject.tag == "Enemy_Bullet" && col.gameObject.tag == "Player")
+
+		//void Update()
+		//{
+
+		//	if (!Bullet_Renderer.isVisible)
+		//	{
+		//		gameObject.SetActive(false);
+		//	}
+		//}
+		private void OnTriggerEnter(Collider col)
 		{
-			gameObject.SetActive(false);
+			//それぞれのキャラクタの弾が敵とプレイヤーにあたっても消えないようにするための処理
+			if ((gameObject.tag == "Enemy_Bullet" && col.gameObject.tag == "Player") || (gameObject.tag == "Player_Bullet" && col.gameObject.tag == "Enemy"))
+			{
+				gameObject.SetActive(false);
 
-			//add:0513_takada 爆発エフェクトのテスト
-			AddExplosionProcess();
+				//add:0513_takada 爆発エフェクトのテスト
+				AddExplosionProcess();
+			}
+			//if (gameObject.tag == "Player_Bullet" && col.gameObject.tag == "Enemy")
+			//{
+			//	gameObject.SetActive(false);
+
+			//	//add:0513_takada 爆発エフェクトのテスト
+			//	AddExplosionProcess();
+			//}
 		}
-		if (gameObject.tag == "Player_Bullet" && col.gameObject.tag == "Enemy")
+
+		private void AddExplosionProcess()
 		{
-			gameObject.SetActive(false);
-
-			//add:0513_takada 爆発エフェクトのテスト
-			AddExplosionProcess();
+			ParticleManagement particleManagementCS;
+			particleManagementCS = GameObject.Find("ParticleManager").GetComponent<ParticleManagement>();
 		}
-	}
 
-	private void AddExplosionProcess()
-	{
-		ParticleManagement particleManagementCS;
-		particleManagementCS = GameObject.Find("ParticleManager").GetComponent<ParticleManagement>();
+		/// <summary>
+		/// 向きの変更
+		/// </summary>
+		/// <param name="_Dir"> 向きたい向き </param>
+		public void Moving_Facing_Preference(Vector3 _Dir)
+		{
+			transform.right = _Dir;
+			Travelling_Direction = _Dir;
+		}
+
+		/// <summary>
+		/// 移動したい向きのみ変更
+		/// </summary>
+		/// <param name="_Dir"></param>
+		public void FacingChange(Vector3 _Dir)
+		{
+			Travelling_Direction = _Dir;
+		}
+
+		/// <summary>
+		/// 向いている方向に移動
+		/// </summary>
+		public void Moving_To_Facing()
+		{
+			Vector3 temp_Pos = transform.right.normalized * shot_speed;
+
+			transform.position += temp_Pos;
+		}
+
+		/// <summary>
+		/// 進行方向に移動
+		/// </summary>
+		public void Moving_To_Travelling_Direction()
+		{
+			Vector3 tempPos = Travelling_Direction.normalized * shot_speed;
+			transform.position += tempPos;
+		}
 	}
 }
---
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/Beam_Bullet.cs
@@ -0,0 +1,23 @@
+﻿//作成日2019/06/13
+// ビーム型バレットの管理
+// 作成者:諸岡勇樹
+/*
+ * 2019/06/06	ビーム型のバレットの動きの制御
+ */
+using UnityEngine;
+using ObjectStatus;
+
+public class Beam_Bullet : bullet_status
+{
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
