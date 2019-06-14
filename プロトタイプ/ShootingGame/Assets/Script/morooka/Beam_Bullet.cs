9db22eaa0da72e8c3e5449e101c95d68164ef1cd
741PC-17\student <cu.common@gmail.com>
2019-06-14 10:55:59 +0900

    途中

--- プロトタイプ/ShootingGame/Assets/Script/morooka/Beam_Bullet.cs
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/Beam_Bullet.cs
@@ -5,17 +5,16 @@
  * 2019/06/06	ビーム型のバレットの動きの制御
  */
 using UnityEngine;
-using ObjectStatus;
 
 public class Beam_Bullet : bullet_status
 {
-	// Start is called before the first frame update
+
+
 	void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
