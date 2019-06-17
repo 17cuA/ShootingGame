0bf6e598ae8ad0f6a7071cb9df14a9b662561504
741PC-17\student <cu.common@gmail.com>
2019-06-17 11:42:35 +0900

    途中

--- プロトタイプ/ShootingGame/Assets/Script/morooka/motakku_Enemy.cs
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/motakku_Enemy.cs
@@ -6,21 +6,31 @@
  */
 using UnityEngine;
 using StorageReference;
-
+using System.Collections.Generic;
 public class motakku_Enemy : character_status
 {
 	private const int Beam_Muzzle_Cnt = 2;		// ビーム型のバレット
 	private const int Bullet_Muzzle_Cnt = 6;
-	private GameObject[] Beam_Mazle { get; set; }		//ビーム型バレットの発射口
-	private GameObject[] Shot_Mazle { get; set; }       //攻撃の発射口に使用
+	private List<GameObject> Beam_Mazle { get; set; }		//ビーム型バレットの発射口
+	private List<GameObject> Shot_Mazle { get; set; }       //攻撃の発射口に使用
 
 	void Start()
     {
-		Beam_Mazle = new GameObject[Beam_Muzzle_Cnt];
-		Shot_Mazle = new GameObject[Bullet_Muzzle_Cnt];
+		Beam_Mazle = new List<GameObject>();
+		Shot_Mazle = new List<GameObject>();
+
 		for (int i = 0; i < transform.childCount; i++)
 		{
-			Shot_Mazle[i] = transform.GetChild(i).gameObject;
+			GameObject obj = transform.GetChild(i).gameObject;
+
+			if(obj.name == "Muzzle_Side")
+			{
+				Beam_Mazle.Add(obj.gameObject);
+			}
+			else if(obj.name == "Muzzle_Front")
+			{
+				Shot_Mazle.Add(obj.gameObject);
+			}
 		}
 	}
 	void Update()
