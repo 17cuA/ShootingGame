1f6b38e0f187a33cb729113d33c82efb29bf200f
741PC-18\student <cu.common@gmail.com>
2019-06-17 14:01:11 +0900

    ツール作業途中（大まかな設定完了）

--- path_Active/No1(mine)/Assets/Script/Enemy_Test.cs
+++ path_Active/No1(mine)/Assets/Script/Enemy_Test.cs
@@ -19,33 +19,36 @@ public class Enemy_Test : MonoBehaviour
 	void Start()
 	{
 		LC = GameObject.Find("Beje").GetComponent<LineRenderer>();
-		//Float_Data 
-		//if (Enemy_Data == null)
-		//{
-		//	Enemy_Data = new Database_Manager(name);
-		//	Float_Data = new float[Enemy_Data.Database_Array.GetLength(0), Enemy_Data.Database_Array.GetLength(1)];
-		//}
-		//for (int i = 0; i < Enemy_Data.Database_Array.GetLength(0); i++)
-		//{
-		//	for (int j = 0; j < Enemy_Data.Database_Array.GetLength(1); j++)
-		//	{
-		//		Float_Data[i, j] = Enemy_Data.ToFloat(i, j);
-		//	}
-		//}
+		if (Enemy_Data == null　|| name != null)
+		{
+			Enemy_Data = new Database_Manager(name);
+			Float_Data = new float[Enemy_Data.Database_Array.GetLength(0), Enemy_Data.Database_Array.GetLength(1)];
+			for (int i = 0; i < Enemy_Data.Database_Array.GetLength(0); i++)
+			{
+				for (int j = 0; j < Enemy_Data.Database_Array.GetLength(1); j++)
+				{
+					Float_Data[i, j] = Enemy_Data.ToFloat(i, j);
+				}
+			}
+
+		}
 	}
 
     // Update is called once per frame
     void Update()
     {
-		transform.position = new Vector3(Float_Data[first, 0], Float_Data[first, 1], Float_Data[first, 2]);
+		if (Enemy_Data == null || name != null)
+		{
+			transform.position = new Vector3(Float_Data[first, 0], Float_Data[first, 1], Float_Data[first, 2]);
 
-		if (first >= Enemy_Data.Database_Array.GetLength(0)-1)
-		{
-			first = 0;
-		}
-		else
-		{
-			first++;
+			if (first >= Enemy_Data.Database_Array.GetLength(0) - 1)
+			{
+				first = 0;
+			}
+			else
+			{
+				first++;
+			}
 		}
     }
 }
