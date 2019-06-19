411bdb845c4698d5da5b428b92f7f61fc9011080
wanarisu <wanarisu@gmail.com>
2019-06-20 07:32:33 +0900

    落下処理完了

--- プロトタイプ/ShootingGame/Assets/Script/morooka/Bullet/Missile.cs
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/Bullet/Missile.cs
@@ -4,40 +4,46 @@
 /*
  * 2019/06/19 落下と地面衝突時の移動向き変更
  */
-
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Missile : bullet_status
 {
-	Vector3 pos;
-	float a = -1.9f;
+	[SerializeField]
+	private Vector3 pos;
+	float a = -0.01f;
 	int num = 0;
 	int flame = 0;
 
-    private void Start()
+    private new void Start()
     {
 		base.Start();
 		pos = transform.position;
 		FacingChange(new Vector3(1.0f,0.0f,0.0f));
     }
 
-	private void Update()
+	private new void Update()
     {
 		base.Update();
-		Vector3 vector = transform.position;
-		vector.y = a * ((flame * shot_speed) - pos.x) * (transform.position.x - pos.x) + pos.y;
 
-		//transform.right = vector;
-		transform.position = vector;
+		if(num == 0)
+		{
+			pos = transform.position;
+			num++;
+		}
 
-		Moving_To_Travelling_Direction();
+		Vector3 vector = Vector3.zero;
+		vector.x = flame;
+		vector.y = (a * Mathf.Pow((int)(vector.x - pos.x), 2)) + pos.y;
+		transform.position += vector.normalized * shot_speed;
+		flame++;
     }
 
 	void OnEnable()
 	{
-		pos = transform.position;
 		flame = 0;
+		num = 0;
 	}
 }
