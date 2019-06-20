db448dcb73b7211bc89c55bac7cd3a4b2aeafeb8
wing1231 <wingfryria@gmail.com>
2019-06-20 16:37:45 +0900

    途中

---
+++ プロトタイプ/ShootingGame/Assets/Script/kawamura/TurnToPlayer_Slow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnToPlayer_Slow : MonoBehaviour
+{
+	public GameObject playerObj; // 注視したいオブジェクトをInspectorから入れておく
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (playerObj == null)
+		{
+			playerObj = GameObject.FindGameObjectWithTag("Player");
+		}
+
+		// 補完スピードを決める
+		float speed = 0.1f;
+		// ターゲット方向のベクトルを取得
+		Vector3 relativePos = playerObj.transform.position - this.transform.position;
+		// 方向を、回転情報に変換
+		Quaternion rotation = Quaternion.LookRotation(relativePos);
+		// 現在の回転情報と、ターゲット方向の回転情報を補完する
+		transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, speed);
+
+	}
+}
+
