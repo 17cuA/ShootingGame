1c60f33ed4e8b879b11ac0fcd3e516834c6c3f38
Shooting 17CU <cu.common@gmail.com>
2019-06-27 10:54:33 +0900

    ミサイルOK

--- プロトタイプ/ShootingGame/Assets/Script/kubota/Player/Player1.cs
+++ プロトタイプ/ShootingGame/Assets/Script/kubota/Player/Player1.cs
@@ -20,6 +20,7 @@ public class Player1 : character_status
 	public float energy;						 //レーザー打つためのエネルギー
 	public float energy_Max;            //エネルギーの最大値
 	private Obj_Storage OS;             //ストレージからバレットの情報取得
+	public GameObject Missilemozle;
 	public enum Bullet_Type　　//弾の種類
 	{
 		Single,
@@ -186,9 +187,9 @@ public class Player1 : character_status
 	}
     	private void Single_Fire()
 	{
-		GameObject obj = Object_Instantiation.Object_Reboot(Game_Master.OBJECT_NAME.ePLAYER_MISSILE, shot_Mazle.transform.position, shot_Mazle.transform.right);
+		GameObject obj = Object_Instantiation.Object_Reboot(Game_Master.OBJECT_NAME.ePLAYER_MISSILE, Missilemozle.transform.position, shot_Mazle.transform.right);
 		obj.GetComponent<Missile>().Setting_On_Reboot(1);
-		obj = Object_Instantiation.Object_Reboot(Game_Master.OBJECT_NAME.ePLAYER_MISSILE, shot_Mazle.transform.position, shot_Mazle.transform.right);
+		obj = Object_Instantiation.Object_Reboot(Game_Master.OBJECT_NAME.ePLAYER_MISSILE, Missilemozle.transform.position, shot_Mazle.transform.right);
 		obj.GetComponent<Missile>().Setting_On_Reboot(0);
 		//GameObject gameObject =Obj_Storage.Storage_Data.PlayerBullet.Active_Obj();
 		//gameObject.transform.rotation *= Direction ;
