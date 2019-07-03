d21d32556998044e43f9ca350364e49d26a41e8d
Shooting 17CU <cu.common@gmail.com>
2019-07-03 19:20:42 +0900

    ミサイルの絵の修正

--- プロトタイプ/ShootingGame/Assets/Script/morooka/Display/Remaining_Display.cs
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/Display/Remaining_Display.cs
@@ -21,6 +21,7 @@ public class Remaining_Display : MonoBehaviour
 
 	private string Font_Path { get; set; }
 	private string Image_Path { get; set; }
+	private List<GameObject> Object_For_Display { get; set; }
 	private List<Image> image_image { get; set; }
 	private Player1 Player_Data { get; set; }
 	public Character_Display Object_To_Display { private set; get; }
@@ -28,46 +29,47 @@ public class Remaining_Display : MonoBehaviour
 	private int Remaining_Num { get; set; }
 
 	public Sprite[] Sprite_Ok { get; set; }
-	public Sprite Sprite_No { get; set; }
-	public int iiiiii;
-
-	// Start is called before the first frame update
-	void One_Start()
-    {
-		Player_Data = Obj_Storage.Storage_Data.Player.Get_Obj()[0].GetComponent<Player1>();
-		Remaining_Num = Player_Data.Remaining;
-
-		//Sprite_Ok =null;
-		Sprite_Ok = Resources.LoadAll<Sprite>("morooka/Remaining");
-		Sprite_No = null;
-
-		image_image = new List<Image>();
-		for (int i = 0; i < transform.childCount; i++)
-		{
-			image_image.Add(transform.GetChild(i).GetComponent<Image>());
-			image_image[i].sprite = Sprite_Ok[0];
-		}
-
-		//for(int i = 0; i<Player_Data.Remaining;i++)
-		//{
-		//	Vector2 pos = image_image[i].rectTransform.position;
-		//	pos.x -= (10.0f * i);
-		//	image_image[i].rectTransform.position = pos;
-
-		//	image_image[i].sprite = Sprite_Ok;
-		//}
-		Object_To_Display = new Character_Display(2, "morooka/SS", gameObject, position);
-		Object_To_Display.Character_Preference("1P");
-		Object_To_Display.Size_Change(new Vector3(font_size, font_size, font_size));
-	}
+	public int One_Play;
 
 	// Update is called once per frame
 	void Update()
     {
-		if(iiiiii == 0)
+		if(One_Play == 0)
 		{
-			One_Start();
-			iiiiii++;
+			Player_Data = Obj_Storage.Storage_Data.Player.Get_Obj()[0].GetComponent<Player1>();
+			Remaining_Num = Player_Data.Remaining;
+
+			Sprite_Ok = Resources.LoadAll<Sprite>("morooka/Remaining");
+
+			Vector2 posTemp = transform.position;
+			posTemp.y = -100;
+
+			Object_For_Display = new List<GameObject>();
+			image_image = new List<Image>();
+			for (int i = 0; i < Remaining_Num; i++)
+			{
+				Object_For_Display.Add(new GameObject());
+				Object_For_Display[i].AddComponent<Image>();
+				image_image.Add(Object_For_Display[i].GetComponent<Image>());
+				Object_For_Display[i].transform.position = posTemp;
+				posTemp.x += 200.0f;
+				Object_For_Display[i].transform.SetParent(transform);
+
+				image_image[i].sprite = Sprite_Ok[0];
+			}
+
+
+			//for (int i = 0; i < transform.childCount; i++)
+			//{
+			//	image_image.Add(transform.GetChild(i).GetComponent<Image>());
+			//	image_image[i].sprite = Sprite_Ok[0];
+			//}
+
+			Object_To_Display = new Character_Display(2, "morooka/SS", gameObject, position);
+			Object_To_Display.Character_Preference("1P");
+			Object_To_Display.Size_Change(new Vector3(font_size, font_size, font_size));
+
+			One_Play++;
 		}
 
 		if(Player_Data.Remaining < Remaining_Num)
