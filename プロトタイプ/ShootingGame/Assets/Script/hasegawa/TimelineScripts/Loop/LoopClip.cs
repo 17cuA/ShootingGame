66727e6b4ee5ee6264e70c77a816c7d1c709856f
741PC-33\student <cu.common@gmail.com>
2019-07-03 13:46:00 +0900

    data add

---
+++ プロトタイプ/ShootingGame/Assets/Script/hasegawa/TimelineScripts/Loop/LoopClip.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * 20190627 作成
+ */
+/* ループを行うクリップの作成 */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class LoopClip : PlayableAsset, ITimelineClipAsset
+{
+	public ClipCaps clipCaps
+	{
+		get { return ClipCaps.None; }
+	}
+
+	public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+	{
+		// 情報の取得
+		ScriptPlayable<LoopBehaviour> playable = ScriptPlayable<LoopBehaviour>.Create(graph);
+		LoopBehaviour behaviour = playable.GetBehaviour();
+		// 情報の設定
+		behaviour.Director = owner.GetComponent<PlayableDirector>();
+		behaviour.WaitLoopTrigger = FindObjectOfType<WaitLoopTrigger>();
+		return playable;
+	}
+}
---
+++ プロトタイプ/ShootingGame/Assets/Script/hasegawa/TimelineScripts/SetTime/SetTimeTrack.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * 20190628 作成
+ */
+/* 設定した任意のフレームまでスキップするクリップを配置するためのトラック */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+[TrackColor(0f, 40f / 255f, 1f)]
+[TrackClipType(typeof(SetTimeClip))]
+public class SetTimeTrack : TrackAsset
+{
+}
