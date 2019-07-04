88af0e765fa2fea9e2ccd51c7068c4e7fc348cb4
Shooting 17CU <cu.common@gmail.com>
2019-07-04 10:49:59 +0900

    統合補助

--- プロトタイプ/ShootingGame/Assets/Script/Chenken/Item/Item.cs
+++ プロトタイプ/ShootingGame/Assets/Script/Chenken/Item/Item.cs
@@ -15,5 +15,15 @@ public class Item : MonoBehaviour
 {
 	[Header("パワーアップタイプ")]
 	public ItemType itemType;
+
+	[SerializeField, Header("移動速度")]
+	private float move_speed;
+
+	private void Update()
+	{
+		Vector3 pos = transform.position;
+		pos.x -= move_speed;
+		transform.position = pos;
+	}
 }
 
