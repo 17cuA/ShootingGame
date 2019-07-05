0ad3d405841acfc11a49fba5356823b56738aac2
741PC-18\student <cu.common@gmail.com>
2019-07-05 15:04:57 +0900

    各種バグ修正中

---
+++ プロトタイプ/ShootingGame/Assets/Script/kubota/Particle/obj_Off.cs
@@ -0,0 +1,15 @@
+﻿//自動でparticleをオフにする機能
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obj_Off : MonoBehaviour
+{
+	ParticleSystem particle;    //particleの情報を取得
+	float cnt_Time;				//生成されてからの時間をカウント
+    void Update()
+    {
+		tim
+    }
+}
