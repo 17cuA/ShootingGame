b3ad55bf94fc0d21991fc7a1ed10c14320c16eb2
741PC-25\student <cu.common@gmail.com>
2019-07-08 16:47:27 +0900

    途中

--- プロトタイプ/ShootingGame/Assets/Script/StartMultiDisplay.cs
+++ プロトタイプ/ShootingGame/Assets/Script/StartMultiDisplay.cs
@@ -1,17 +1,22 @@
-﻿//実行データでマルチディスプレイの認識をさせるスクリプト
-
+﻿//実行データの初期設定
+//
 using UnityEngine;
 using System.Collections;
 
 public class StartMultiDisplay : MonoBehaviour
 {
 
-    void Start()
+    void Awake()
     {
         int maxDisplayCount = 2;
         for (int i = 0; i < maxDisplayCount && i < Display.displays.Length; i++)
         {
             Display.displays[i].Activate();
+			
         }
-    }
+
+
+		//QualitySettings.SetQualityLevel((int)QualityLevel.Good);
+		//Screen.SetResolution(3840, 1080, Screen.fullScreen);
+	}
 }
