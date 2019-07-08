dc8936c85252775cbe8a381638748156cedb15f9
741PC-23\student <cu.common@gmail.com>
2019-07-08 12:27:11 +0900

    パワーリセット条件判断処理　　　追加設定 パワーリセット処理　　　　　　　追加設定 オプションパワー　　　　　　　　修正

--- プロトタイプ/ShootingGame/Assets/Script/Chenken/TempPlayer.cs
+++ プロトタイプ/ShootingGame/Assets/Script/Chenken/TempPlayer.cs
@@ -17,28 +17,34 @@ public class TempPlayer : MonoBehaviour
 	public void Awake()
 	{
 		player1 = GetComponent<Player1>();
+
 	}
 	//プレイヤーがアクティブになった瞬間に呼び出される
 	private void OnEnable()
 	{
 		//第一引数は指定パワーアップ、第二引数はリセット条件、第三引数はリセット処理
-		PowerManager.Instance.AddCheckFunction(PowerManager.Power.PowerType.SPEEDUP, () => { return player1.hp < 1; }, null);
-		PowerManager.Instance.AddCheckFunction(PowerManager.Power.PowerType.MISSILE, () => { return player1.hp < 1; }, null);
-		PowerManager.Instance.AddCheckFunction(PowerManager.Power.PowerType.DOUBLE,  () => { return player1.hp < 1 || player1.bullet_Type == Player1.Bullet_Type.Laser; }, null);
-		PowerManager.Instance.AddCheckFunction(PowerManager.Power.PowerType.LASER,   () => { return player1.hp < 1 || player1.bullet_Type == Player1.Bullet_Type.Double; }, null);
-		PowerManager.Instance.AddCheckFunction(PowerManager.Power.PowerType.SHIELD,  () => { return player1.shield < 1; }, null);
+		//第二引数の条件が成立するなら、第三引数の処理が実行される
+		PowerManager.Instance.AddCheckFunction(PowerManager.Power.PowerType.SPEEDUP, () => { return player1.hp < 1; }, () => { Debug.Log("スピードアップリセット"); });
+		PowerManager.Instance.AddCheckFunction(PowerManager.Power.PowerType.MISSILE,  () => { return player1.hp < 1; }, () => { Debug.Log("ミサイルリセット"); });
+		PowerManager.Instance.AddCheckFunction(PowerManager.Power.PowerType.DOUBLE,  () => { return player1.hp < 1 || player1.bullet_Type == Player1.Bullet_Type.Laser; }, () => { Debug.Log("ダブルリセット"); });
+		PowerManager.Instance.AddCheckFunction(PowerManager.Power.PowerType.LASER,     () => { return player1.hp < 1 || player1.bullet_Type == Player1.Bullet_Type.Double; }, () => { Debug.Log("レーサーリセット"); });
+		PowerManager.Instance.AddCheckFunction(PowerManager.Power.PowerType.SHIELD,   () => { return player1.shield < 1; }, () => { Debug.Log("シールドリセット"); });
 	}
 	//プレイヤーのアクティブが切られたら呼び出される
 	private void OnDisable()
 	{
-
-
+		PowerManager.Instance.RemoveCheckFunction(PowerManager.Power.PowerType.SPEEDUP, () => { return player1.hp < 1; }, () => { Debug.Log("スピードアップリセット"); });
+		PowerManager.Instance.RemoveCheckFunction(PowerManager.Power.PowerType.MISSILE, () => { return player1.hp < 1; }, () => { Debug.Log("ミサイルリセット"); });
+		PowerManager.Instance.RemoveCheckFunction(PowerManager.Power.PowerType.DOUBLE, () => { return player1.hp < 1 || player1.bullet_Type == Player1.Bullet_Type.Laser; }, () => { Debug.Log("ダブルリセット"); });
+		PowerManager.Instance.RemoveCheckFunction(PowerManager.Power.PowerType.LASER, () => { return player1.hp < 1 || player1.bullet_Type == Player1.Bullet_Type.Double; }, () => { Debug.Log("レーサーリセット"); });
+		PowerManager.Instance.RemoveCheckFunction(PowerManager.Power.PowerType.SHIELD, () => { return player1.shield < 1; }, () => { Debug.Log("シールドリセット"); });
 	}
 	
 
 	void Update()
 	{
 		PowerManager.Instance.Update();
+		//ビットン数をパワーマネージャーに更新する
 		PowerManager.Instance.UpdateBit(player1.bitIndex);
 		//---------------------------
 		//パワーマネージャー更新
