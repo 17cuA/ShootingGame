440b6afe9e915c82c1c18e2df73067ad6ef922a4
kawamura727 <dqseurat727@mineo.jp>
2019-07-11 23:55:20 +0900

    アイテムの敵修正途中

--- プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy/DropItem.cs
+++ プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy/DropItem.cs
@@ -3,19 +3,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using StorageReference;
 
 public class DropItem : MonoBehaviour
 {
 	public GameObject item;
 	Vector3 itemPos;
-	bool isQuitting=false;
-	void Start()
-    {
-		item = Resources.Load("Item/Item_Test") as GameObject;
+    public bool isQuitting = false;
 
+    private void Awake()
+    {
+        item = Resources.Load("Item/Item_Test") as GameObject;
+        isQuitting = true;
+    }
+
+    private void OnEnable()
+    {
+        isQuitting = false;
+    }
+
+    void Start()
+    {
+        isQuitting = true;
 	}
 
-	void Update()
+
+    void Update()
     {
 		//アイテムの生成位置更新
 		itemPos = transform.position;
@@ -30,6 +43,7 @@ public class DropItem : MonoBehaviour
 		if(!isQuitting)
         {
             Instantiate(item, itemPos, transform.rotation);
+            //Object_Instantiation.Object_Reboot("PowerUP_Item", itemPos, transform.rotation);
         }
     }
 
