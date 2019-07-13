30c7afb75793ed50349361660c90370eafbb58bc
kawamura727 <dqseurat727@mineo.jp>
2019-07-14 06:26:05 +0900

    途中

--- プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy/Enemy_ZerosForce.cs
+++ プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy/Enemy_ZerosForce.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Enemy_ZerosForce : character_status
 {
 	public int saveHP;
-
+    public int saveHP2;
 
 	float scale;
 	float scaleNum;
@@ -14,54 +14,67 @@ public class Enemy_ZerosForce : character_status
 	public bool isSmall;
 	public bool isBig;
 	public bool isDamage = false;
+    public bool isNoDamage;
+
     void Start()
     {
 		isBig = true;
-		scale = 3.0f;
+        isNoDamage = true;
+        scale = 3.0f;
 		scaleNum = 0.05f;
 		scaleNum_Maximum = 0.007f;
 		noDamageTime = 0;
 
 		HP_Setting();
 		saveHP = hp;
+        saveHP2 = hp;
     }
 
     void Update()
     {
+        if(saveHP2>hp)
+        {
+            isDamage = true;
+            saveHP2 = hp;
+            noDamageTime = 0;
+        }
 		if (saveHP > hp)
 		{
 			isDamage = true;
 			saveHP = hp;
 		}
 
-		if (isDamage && noDamageTime <= 30)
-		{
-			scale -= scaleNum;
-			if (scale < 3.0f)
-			{
-				scale = 3.0f;
-			}
-		}
+        if (isDamage && noDamageTime <= 30)
+        {
+            scale -= scaleNum;
+            if (scale < 3.0f)
+            {
+                scale = 3.0f;
+            }
+        }
+        else if (noDamageTime > 30 && scale < 9.0f)
+        {
+            hp++;
+            if (hp > 100)
+            {
+                hp = 100;
+            }
+            saveHP = hp;
+            saveHP2 = hp;
+            scale += scaleNum;
+        }
 
-		if (saveHP == hp)
+
+        if (saveHP2 == hp)
 		{
 			noDamageTime++;
 		}
 		else
 		{
-			noDamageTime = 0;
-			saveHP = hp;
+			//noDamageTime = 0;
+			//saveHP = hp;
 		}
 
-		if (noDamageTime > 30 && scale < 9.0f)
-		{
-			hp++;
-			if (hp > 100)
-			{
-				hp = 100;
-			}
-			scale += scaleNum;
-		}
 
 		if (scale >= 9.0f)
 		{
