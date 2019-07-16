25650ef57d549888cf64bbe00daadc00e43de373
741PC-37\student <cu.common@gmail.com>
2019-07-16 14:12:33 +0900

    パーティクル変更（仮）

--- プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy/EnemyGroupManage.cs
+++ プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy/EnemyGroupManage.cs
@@ -40,8 +40,8 @@ public class EnemyGroupManage : MonoBehaviour
 			itemTransform = null;
 			remainingEnemiesCnt = childNum;
 
-			Destroy(this.gameObject);
-			//gameObject.SetActive(false);
+			//Destroy(this.gameObject);
+			gameObject.SetActive(false);
 
 			//isDead = true;
 			//Died_Process();
