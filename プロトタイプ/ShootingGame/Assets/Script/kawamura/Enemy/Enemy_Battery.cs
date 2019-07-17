bae23318619962b5324c4285dbfb340f3ad8cfc4
741PC-35\student <cu.common@gmail.com>
2019-07-17 12:08:26 +0900

    隕石と砲台

---
+++ プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy/Enemy_Battery.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Battery : character_status
+{
+    new void Start()
+    {
+		HP_Setting();
+		base.Start();
+    }
+
+    void Update()
+    {
+		if (hp < 1)
+		{
+			Died_Process();
+		}
+    }
+}
