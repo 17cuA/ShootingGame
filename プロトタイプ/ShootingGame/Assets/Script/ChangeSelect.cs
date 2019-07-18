c55acdc9f2bf52de63d4c4a3f72fbcf93bdc5eb7
wing1231 <wingfryria@gmail.com>
2019-07-18 11:26:52 +0900

    マージ用調整

--- プロトタイプ/ShootingGame/Assets/Script/ChangeSelect.cs
+++ プロトタイプ/ShootingGame/Assets/Script/ChangeSelect.cs
@@ -8,11 +8,7 @@ public class ChangeSelect : MonoBehaviour
 
 	public void SelectSelf()
 	{
-		//クリックされた時 かつ lockStateがLockedではない時だけ実行
-		if (Input.GetMouseButtonDown(0) && Cursor.lockState != CursorLockMode.Locked)
-		{
-			EventSystem.current.SetSelectedGameObject(gameObject);
-		}
+		EventSystem.current.SetSelectedGameObject(gameObject);
 	}
 
 	public void NonSelectSelf()
