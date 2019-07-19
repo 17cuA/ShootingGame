ee249ae4847f819334c34a182317ecedd530c022
741PC-19\student <cu.common@gmail.com>
2019-07-19 18:57:23 +0900

    パーティクル直線　修正

--- プロトタイプ/ShootingGame/Assets/Script/Chenken/LaserEmitter.cs
+++ プロトタイプ/ShootingGame/Assets/Script/Chenken/LaserEmitter.cs
@@ -90,7 +90,6 @@ public class LaserEmitter : MonoBehaviour
         laserGo.transform.localPosition = Vector3.zero;
         this.currentLaser =  newLaser;
         this.lasers.Add(currentLaser); 
-		
     }
 
     private void LaunchLaserContinous()
