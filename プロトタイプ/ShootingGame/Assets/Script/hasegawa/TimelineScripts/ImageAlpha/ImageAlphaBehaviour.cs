1472c38321eb80b31341b8016bc7eb841e3a83c4
741PC-36\student <cu.common@gmail.com>
2019-07-19 15:27:45 +0900

    球体背景の修正

---
+++ プロトタイプ/ShootingGame/Assets/Script/hasegawa/TimelineScripts/ImageAlpha/ImageAlphaBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+[System.Serializable]
+public class ImageAlphaBehaviour : PlayableBehaviour
+{
+	[SerializeField, Range(0f, 1f)] float alpha;
+	public float Alpha { set { alpha = value; } get { return alpha; } }
+}
