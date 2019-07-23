c1c4e378a22a7cb1b20c68db0c2c4db3f3385349
Shooting 17CU <cu.common@gmail.com>
2019-07-23 12:30:44 +0900

    アイコン位置調整

--- プロトタイプ/ShootingGame/Assets/Script/morooka/Display/Remaining_Display.cs
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/Display/Remaining_Display.cs
@@ -18,11 +18,17 @@ public class Remaining_Display : MonoBehaviour
 	[SerializeField]
 	[Header("表示位置")]
 	private Vector3 position;
+	[SerializeField,Header("表示位置2")]
+	private Vector3 lifePosition;
+	[SerializeField,Header("表示位置3")]
+	private Vector3 xRPosition;
 
-	private List<GameObject> Remaining_Object { get; set; }         // 残機アイコン表示用オブジェクト
-																	//private List<Image> Remaining_Object_Image { get; set; }        // 残機アイコン表示用 Image 
-	private Player1 Player_Data { get; set; }                       // プレイヤーの残機確認用
+	//private List<GameObject> Remaining_Object { get; set; }         // 残機アイコン表示用オブジェクト
+	//private List<Image> Remaining_Object_Image { get; set; }        // 残機アイコン表示用 Image 
+	private GameObject[] oya;
+	private Player1 Player_Data { get; set; }								// プレイヤーの残機確認用
 	private Character_Display Object_To_Display { set; get; }       // 文字表示用
+	private Character_Display[] Life;							// ライフ表示
 	private int Remaining_Num { get; set; }                         // 現在表示してる残機
 	private Sprite[] Display_Sprite { get; set; }                   // 表示したいスプライト用
 	private string Temp_String{get;set;}
@@ -32,6 +38,11 @@ public class Remaining_Display : MonoBehaviour
 		// プレイヤーの情報がないとき
 		if(Player_Data == null)
 		{
+			oya = new GameObject[3] { new GameObject(),new GameObject(),new GameObject()};
+			foreach(GameObject obj in oya)
+			{
+				obj.transform.parent = transform;
+			}
 			// プレイヤーのデータを保存
 			Player_Data = Obj_Storage.Storage_Data.Player.Get_Obj()[0].GetComponent<Player1>();
 			Remaining_Num = Player_Data.Remaining;
@@ -44,28 +55,38 @@ public class Remaining_Display : MonoBehaviour
 
 			// アイコンの作製と設置
 			// プレイヤーの初期残機数からアイコン生成
-			Remaining_Object = new List<GameObject>();
+			//Remaining_Object = new List<GameObject>();
 			//Remaining_Object_Image = new List<Image>();
 			for (int i = 0; i < Remaining_Num; i++)
 			{
-				Remaining_Object.Add(new GameObject());
-				Remaining_Object[i].AddComponent<Image>();
+				//Remaining_Object.Add(new GameObject());
+				//Remaining_Object[i].AddComponent<Image>();
 				//Remaining_Object_Image.Add(Remaining_Object[i].GetComponent<Image>());
-				Remaining_Object[i].transform.SetParent(transform);
+				//Remaining_Object[i].transform.SetParent(transform);
 
-				RectTransform r_transform = Remaining_Object[i].GetComponent<RectTransform>();
-				r_transform.localPosition = posTemp;
-				r_transform.localScale *= 1.4f;
+				//RectTransform r_transform = Remaining_Object[i].GetComponent<RectTransform>();
+				//r_transform.localPosition = posTemp;
+				//r_transform.localScale *= 1.4f;
 
 				posTemp.x += 150.0f;
 				//Remaining_Object_Image[i].sprite = Display_Sprite[0];
 			}
 
 			// 「1P」の文字作製と設置
-			string temp = "1P__LIFE_X_" + Remaining_Num.ToString();
-			Object_To_Display = new Character_Display(temp.Length, "morooka/SS", gameObject, position);
-			Object_To_Display.Character_Preference(temp);
+			Temp_String = "1P______________LIFE";
+			Object_To_Display = new Character_Display(Temp_String.Length, "morooka/SS", oya[0], position);
+			Object_To_Display.Character_Preference(Temp_String);
 			Object_To_Display.Size_Change(new Vector3(font_size, font_size, font_size));
+
+			Life = new Character_Display[2];
+			Temp_String = "_X_";
+			Life[0] = new Character_Display(Temp_String.Length, "morooka/SS", oya[1], lifePosition);
+			Life[0].Character_Preference(Temp_String);
+			Life[0].Size_Change(new Vector3(font_size / 3.0f * 2.0f, font_size / 3.0f * 2.0f, font_size / 3.0f * 2.0f));
+
+			Life[1] = new Character_Display(1, "morooka/SS", oya[2],xRPosition);
+			Life[1].Character_Preference(Remaining_Num.ToString());
+			Life[1].Size_Change(new Vector3(font_size, font_size, font_size));
 		}
 
 		//表示中のアイコンの数とプレイヤーの残機の数が違うとき
@@ -73,7 +94,7 @@ public class Remaining_Display : MonoBehaviour
 		{
 			// 表示中のアイコンの非表示化
 			Remaining_Num = Player_Data.Remaining;
-
+			Life[1].Character_Preference(Remaining_Num.ToString());
 			//Remaining_Object_Image[Remaining_Num].enabled = false;
 		}
     }
