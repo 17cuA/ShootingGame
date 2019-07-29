f9186a1bafe88a19d815214fba2776e39ad6dc69
741PC-26\student <cu.common@gmail.com>
2019-07-29 14:23:28 +0900

    グリッド

--- プロトタイプ/ShootingGame/Assets/Script/kawamura/Grid.cs
+++ プロトタイプ/ShootingGame/Assets/Script/kawamura/Grid.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Grid : MonoBehaviour
 {
 
-	//Vector3 MOVEX = new Vector3(0.175f, 0, 0); // x軸方向に１マス移動するときの距離
+	Vector3 MOVEX = new Vector3(0.175f, 0, 0); // x軸方向に１マス移動するときの距離
 	Vector3 MOVEY = new Vector3(0, 0.175f, 0); // y軸方向に１マス移動するときの距離
-
+	public float x;            //x軸の入力
+	public float y;
+	public float speed;
 	public float step = 2f;     // 移動速度
-	Vector3 target;      // 入力受付時、移動後の位置を算出して保存 
+	public Vector3 target;      // 入力受付時、移動後の位置を算出して保存 
 	Vector3 prevPos;     // 何らかの理由で移動できなかった場合、元の位置に戻すため移動前の位置を保存
+	Vector3 vector3;
 
-
+	public bool isInpt = false;
 
 	// Use this for initialization
 	void Start()
@@ -23,10 +26,10 @@ public class Grid : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-
 		// ① 移動中かどうかの判定。移動中でなければ入力を受付
 		if (transform.position == target)
 		{
+			//MoveX();
 			SetTargetPosition();
 		}
 		Move();
@@ -35,33 +38,61 @@ public class Grid : MonoBehaviour
 	// ② 入力に応じて移動後の位置を算出
 	void SetTargetPosition()
 	{
+		x = Input.GetAxis("Horizontal");            //x軸の入力
+		y = Input.GetAxis("Vertical");              //y軸の入力
 
 		prevPos = target;
 
-		//if (Input.GetKey(KeyCode.RightArrow))
-		//{
-		//	target = transform.position + MOVEX;
-		//	SetAnimationParam(1);
-		//	return;
-		//}
-		//if (Input.GetKey(KeyCode.LeftArrow))
-		//{
-		//	target = transform.position - MOVEX;
-		//	SetAnimationParam(2);
-		//	return;
-		//}
-		if (Input.GetKey(KeyCode.UpArrow) || Input.GetAxis("Vertical") > 0)
+
+		//右上
+		if (x > 0 && y > 0)
+		{
+			target = transform.position + MOVEX + MOVEY;
+
+		}
+		//右下
+		else if (x > 0 && y < 0)
+		{
+			target = transform.position + MOVEX - MOVEY;
+
+		}
+		//左下
+		else if (x < 0 && y < 0)
+		{
+			target = transform.position - MOVEX - MOVEY;
+
+		}
+		//左上
+		else if (x < 0 && y > 0)
+		{
+			target = transform.position - MOVEX + MOVEY;
+
+		}
+		//上
+		else if (y > 0)
 		{
 			target = transform.position + MOVEY;
-			//SetAnimationParam(3);
-			return;
+
 		}
-		if (Input.GetKey(KeyCode.DownArrow) || Input.GetAxis("Vertical") < 0)
+		//右
+		else if (x > 0)
+		{
+			target = transform.position + MOVEX;
+
+		}
+		//下
+		else if (y < 0)
 		{
 			target = transform.position - MOVEY;
-			//SetAnimationParam(0);
-			return;
+
 		}
+		//左
+		else if (x < 0)
+		{
+			target = transform.position - MOVEX;
+
+		}
+
 	}
 
 	// WalkParam  0;下移動　1;右移動　2:左移動　3:上移動
@@ -74,5 +105,19 @@ public class Grid : MonoBehaviour
 	void Move()
 	{
 		transform.position = Vector3.MoveTowards(transform.position, target, step * Time.deltaTime);
+	}
+	void MoveX()
+	{
+													//プレイヤーの移動に上下左右制限を設ける
+		if (transform.position.y >= 4.5f && y > 0) y = 0;
+		if (transform.position.y <= -4.5f && y < 0) y = 0;
+		if (transform.position.x >= 17.0f && x > 0) x = 0;
+		if (transform.position.x <= -17.0f && x < 0) x = 0;
+
+		vector3 = new Vector3(x, y, 0);     //移動のベクトルをvector3に入れる
+
+		//位置情報の更新
+		transform.position = transform.position + vector3 * Time.deltaTime * speed;
+
 	}
 }
