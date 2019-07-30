38a272c8ba82b350105ad82711b28be81478d343
741PC-36\student <cu.common@gmail.com>
2019-07-30 14:07:52 +0900

    背景の星、途中

--- プロトタイプ/ShootingGame/Assets/Script/hasegawa/ShootingStarAnimator.cs
+++ プロトタイプ/ShootingGame/Assets/Script/hasegawa/ShootingStarAnimator.cs
@@ -13,6 +13,7 @@ public class ShootingStarAnimator : MonoBehaviour
 
 	void Start()
 	{
+		// シーンに存在する流れ星を取得していく
 		starParentObject = new GameObject("StarObjects");
 		Object[] array = FindObjectsOfType<ShootingStarBehaviour>();
 		foreach(Object a in array)
@@ -20,12 +21,26 @@ public class ShootingStarAnimator : MonoBehaviour
 			if (animationObjectList.Contains(a as ShootingStarBehaviour)) { continue; }
 			animationObjectList.Add(a as ShootingStarBehaviour);
 			(a as ShootingStarBehaviour).transform.parent = starParentObject.transform;
+			(a as ShootingStarBehaviour).SettingState();
 		}
 	}
+	/// <summary>
+	/// 設定されている流れ星の中からランダムな流れ星を流す
+	/// </summary>
 	public void AnimationRandomShootingStar()
 	{
 		if (animationObjectList.Count <= 0) { return; }
 		int randomNum = Random.Range(0, animationObjectList.Count);
+		int i;
+		// すでにアニメーションをしていたらほかの流れ星にする
+		for (i = 0; i < animationObjectList.Count; ++i)
+		{
+			if (!animationObjectList[randomNum].IsAnimation) { break; }
+			++randomNum;
+			if (randomNum >= animationObjectList.Count) { randomNum = 0; }
+		}
+		// 全ての流れ星が流れていたら流さない
+		if (i >= animationObjectList.Count) { return; }
 		animationObjectList[randomNum].Activate();
 	}
 }
