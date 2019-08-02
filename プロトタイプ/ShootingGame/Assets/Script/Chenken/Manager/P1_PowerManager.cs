64091d7e662fb571940b7e7a089ad7992fed9cf8
741PC-23\student <cu.common@gmail.com>
2019-08-02 10:32:00 +0900

    P2 powerManager

--- プロトタイプ/ShootingGame/Assets/Script/Chenken/Manager/P1_PowerManager.cs
+++ プロトタイプ/ShootingGame/Assets/Script/Chenken/Manager/P1_PowerManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Power
 {
-	public class P2_PowerManager : Singleton<PowerManager>
+	public class P1_PowerManager : Singleton<PowerManager>
 	{
 		/// <summary>
 		/// パワークラス
