4fff6aff3df881d798313a95f14249c87974f1f5
741PC-34\student <cu.common@gmail.com>
2019-09-02 19:08:50 +0900

    せーぶ

--- プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy/Enemy_BeetleRoll.cs
+++ プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy/Enemy_BeetleRoll.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
+﻿//作成者：川村良太
+//ビートル敵の回転スクリプト
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy_BeetleRoll : MonoBehaviour
 {
-    // Start is called before the first frame update
+	public int rollCnt;							//回転した回数を数える
+	public float RotaZ;							//代入する角度の値
+	public float RotaZ_Max;					//回転する最大値
+	[Header("入力用　角度を変える数値の大きさ設定")]
+	public float RotaZ_ChangeValue;     //角度を変える値
+
+	public bool isRoll;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
