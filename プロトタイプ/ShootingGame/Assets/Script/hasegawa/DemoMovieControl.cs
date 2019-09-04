9e228dcc9c65d84c4631fcc16e808414f38d2f8a
741PC-36\student <cu.common@gmail.com>
2019-09-04 18:05:30 +0900

    デモの途中

--- プロトタイプ/ShootingGame/Assets/Script/hasegawa/DemoMovieControl.cs
+++ プロトタイプ/ShootingGame/Assets/Script/hasegawa/DemoMovieControl.cs
@@ -21,12 +21,14 @@ public class DemoMovieControl : MonoBehaviour
 	}
 
 	[SerializeField] VideoPlayer videoPlayer;
-	MoviePlayState playState;
+	[SerializeField] RawImage videoRawImage;
+	MoviePlayState playState = MoviePlayState.eStop;
 	int frame = 0;
 	[SerializeField, Tooltip("ムービーに遷移するまでの秒数")] float transitionTime = 0;
-	int TransitionFrame { get { return TransitionFrame * 60; } }
+	int TransitionFrame { get { return (int)(transitionTime * 60f); } }
 	[SerializeField, Tooltip("画面を覆うImage")] Image displayPlane;
-	bool isPlayingMovie = false;
+	bool IsPlayingMovie { get { return playState == MoviePlayState.ePlaying; } }
+	bool isInput = false;
 
 	void Start()
 	{
@@ -34,6 +36,17 @@ public class DemoMovieControl : MonoBehaviour
 		if (!videoPlayer) { Debug.LogError("Not set to a video of my field!"); return; }
 		// ムービーを停止状態にする
 		videoPlayer.Stop();
+		videoPlayer.gameObject.SetActive(false);
+		// RawImageがセットされていなかったらvideoPlayerと同じRenderTextureがセットされているのを取得する
+		if (!videoRawImage)
+		{
+			RenderTexture setRenderTexture = videoPlayer.targetTexture;
+			RawImage[] getRawImage = FindObjectsOfType<RawImage>();
+			foreach (RawImage rawImage in getRawImage)
+			{
+				if (rawImage.texture == setRenderTexture) { videoRawImage = rawImage; }
+			}
+		}
 		// Imageが設定されていなかったら生成する
 		if (!displayPlane)
 		{
@@ -41,10 +54,11 @@ public class DemoMovieControl : MonoBehaviour
 			displayPlane = plane.AddComponent<Image>();
 			Canvas anyCanvas = FindObjectOfType<Canvas>();
 			displayPlane.transform.parent = anyCanvas.transform;
-			displayPlane.rectTransform.position = Vector2.zero;
+			displayPlane.rectTransform.localPosition = Vector2.zero;
 			displayPlane.rectTransform.sizeDelta = new Vector2(3840f, 1080f);
 			displayPlane.color = Color.black;
 		}
+		displayPlane.color = new Color(displayPlane.color.r, displayPlane.color.g, displayPlane.color.b, 0f);
 	}
 
 	void Update()
@@ -66,20 +80,30 @@ public class DemoMovieControl : MonoBehaviour
 	/// </summary>
 	void PlayBehaviour()
 	{
+		if (Input.anyKeyDown)
+		{
+			isInput = true;
+		}
 		// ムービーが終わりに近づくにつれて暗転させる
-		if ((ulong)videoPlayer.frame >= videoPlayer.frameCount - 10)
+		if ((ulong)videoPlayer.frame >= videoPlayer.frameCount - 10 || isInput)
 		{
 			displayPlane.color += new Color(0f, 0f, 0f, 5f / 60f);
 		}
-		else
+		else if (frame < 12)
 		{
 			displayPlane.color -= new Color(0f, 0f, 0f, 5f / 60f);
+			if (displayPlane.color.a < 0f)
+			{
+				displayPlane.color = new Color(displayPlane.color.r, displayPlane.color.g, displayPlane.color.b, 0f);
+			}
 		}
 		// 完全に暗転したらムービーを停止させる
 		if (displayPlane.color.a >= 1f)
 		{
-			frame = 0;
+			playState = MoviePlayState.eStop;
 			videoPlayer.Stop();
+			isInput = false;
+			frame = 0;
 		}
 	}
 
@@ -93,14 +117,20 @@ public class DemoMovieControl : MonoBehaviour
 		{
 			displayPlane.color += new Color(0f, 0f, 0f, 5f / 60f);
 		}
-		else
+		else if (frame < 12)
 		{
 			displayPlane.color -= new Color(0f, 0f, 0f, 5f / 60f);
+			if (displayPlane.color.a < 0f)
+			{
+				displayPlane.color = new Color(displayPlane.color.r, displayPlane.color.g, displayPlane.color.b, 0f);
+			}
 		}
 		// 暗転したらムービーを流す
 		if (displayPlane.color.a >= 1f)
 		{
 			playState = MoviePlayState.ePlaying;
+			videoPlayer.gameObject.SetActive(true);
+			videoPlayer.Play();
 			frame = 0;
 		}
 	}
