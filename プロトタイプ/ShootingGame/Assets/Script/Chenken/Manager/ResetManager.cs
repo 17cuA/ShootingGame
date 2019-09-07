256dc53bc7151971faeaff6f4a7e7948dab038af
chenkenZzz <d512668247@gmail.com>
2019-09-07 20:29:18 +0900

    二週間InitSpeedバグ

---
+++ プロトタイプ/ShootingGame/Assets/Script/Chenken/Manager/ResetManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Power;
+
+public class ResetManager : MonoBehaviour
+{
+    void Start()
+    {
+        P1_PowerManager.Instance.ResetAllPowerUpgradeCount();
+        P2_PowerManager.Instance.ResetAllPowerUpgradeCount();
+
+        P1_PowerManager.Instance.RemoveAllCheckCallBack();
+        P2_PowerManager.Instance.RemoveAllCheckCallBack();
+
+        P1_PowerManager.Instance.RemoveAllUpdateCallBack();
+        P2_PowerManager.Instance.RemoveAllUpdateCallBack();
+    }
+}
