0c402202fc7dd57cf864db05e4a62c6dce8060c9
741PC-22\student <cu.common@gmail.com>
2019-09-10 12:54:37 +0900

    Title Fade Out

--- プロトタイプ/ShootingGame/Assets/Script/Chenken/Helper_BGMTranstion.cs
+++ プロトタイプ/ShootingGame/Assets/Script/Chenken/Helper_BGMTranstion.cs
@@ -20,7 +20,7 @@ public class Helper_BGMTranstion : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
