6734dfc85e34ce6a7de3a2149627cee669671902
741PC-22\student <cu.common@gmail.com>
2019-09-10 13:27:32 +0900

    BGM切り替え

--- プロトタイプ/ShootingGame/Assets/Script/Chenken/Helper_BGMTranstion.cs
+++ プロトタイプ/ShootingGame/Assets/Script/Chenken/Helper_BGMTranstion.cs
@@ -17,15 +17,68 @@ public class Helper_BGMTranstion : MonoBehaviour
 	[SerializeField] private float fadeOutOverVolume;
 	[SerializeField] private float fadeOutTime;
 	private float fadeOutTimer;
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
+	[SerializeField] private bool isFadeOut = false;
 
-    // Update is called once per frame
-    void Update()
+	private int currentWirelessNumber = 0;
+	private AudioSource audioSource;
+
+	private void Awake()
+	{
+		audioSource = GetComponent<AudioSource>();
+		isFadeIn = true;
+		audioSource.clip = startBGMClip;
+		audioSource.volume = fadeInStartVolume;
+	}
+
+	// Update is called once per frame
+	private void Update()
     {
-        
+		//Fade in / Fade out
+        if(isFadeIn)
+		{
+			fadeInTimer += Time.deltaTime;
+			audioSource.volume = Mathf.Lerp(fadeInStartVolume, 1.0f, fadeInTimer / fadeInTime);
+			if(fadeInTimer >= fadeInTime)
+			{
+				isFadeIn = false;
+				fadeInTimer = 0;
+			}
+		}
+
+		if(isFadeOut)
+		{
+			fadeOutTimer += Time.deltaTime;
+			audioSource.volume = Mathf.Lerp(1.0f, fadeOutOverVolume, fadeOutTimer / fadeOutTime);
+			if(fadeOutTimer >= fadeOutTime)
+			{
+				isFadeOut = false;
+				fadeOutTimer = 0;
+				if(currentWirelessNumber == 1)
+				{
+					audioSource.clip = oneBossBGMClip;
+					isFadeIn = true;
+					audioSource.Play();
+				}
+				else if(currentWirelessNumber == 2)
+				{
+					audioSource.clip = oneBossOverBGMClip;
+					isFadeIn = true;
+					audioSource.Play();
+				}
+				else if(currentWirelessNumber == 3)
+				{
+					audioSource.clip = twoBossBGMClip;
+					isFadeIn = true;
+					audioSource.Play();
+				}
+			}
+		}
+
+		//位置
+		if(Wireless_sinario.Is_using_wireless)
+		{
+			currentWirelessNumber++;
+			isFadeOut = true;
+		}
     }
 }
