19e29d1853e889b8d4f6081eb2eda2d0cb38e418
chenkenZzz <d512668247@gmail.com>
2019-09-11 20:59:22 +0900

    途中

--- プロトタイプ/ShootingGame/Assets/Script/Chenken/LaserLine.cs
+++ プロトタイプ/ShootingGame/Assets/Script/Chenken/LaserLine.cs
@@ -81,4 +81,51 @@ public class LaserLine : Player_Bullet
 		//rigidbody.velocity = new Vector3(shot_speed * 40, 0, 0);
 		base.Moving_To_Travelling_Direction();
 	}
+
+    public new void OnTriggerEnter(Collider col)
+    {      
+		if(gameObject.tag == "Player_Bullet " && col.name == "Bacula")
+		{
+			character_status obj = col.GetComponent<character_status>();
+			if (obj != null)
+			{
+				obj.Opponent = Player_Number;
+			}
+			GameObject effect = Obj_Storage.Storage_Data.Effects[11].Active_Obj();
+			ParticleSystem particle = effect.GetComponent<ParticleSystem>();
+			effect.transform.position = gameObject.transform.position;
+			particle.Play();
+			gameObject.SetActive(false);
+			Player_Bullet_Des();
+		}
+        else if (gameObject.tag == "Player_Bullet" && col.gameObject.tag == "Enemy" && col.gameObject.name != "One_Boss_LaserLine" && col.gameObject.name != "Two_Boss_Laser")
+        {
+            //add:0513_takada 爆発エフェクトのテスト
+            //AddExplosionProcess();
+            character_status obj = col.GetComponent<character_status>();
+            if (obj != null)
+            {
+                obj.Opponent = Player_Number;
+            }
+            GameObject effect = Obj_Storage.Storage_Data.Effects[11].Active_Obj();
+            ParticleSystem particle = effect.GetComponent<ParticleSystem>();
+            effect.transform.position = gameObject.transform.position;
+            particle.Play();
+			Player_Bullet_Des();
+			//if (P1 != null) P1.Bullet_cnt--;
+			//if (Game_Master.Number_Of_People == Game_Master.PLAYER_NUM.eTWO_PLAYER)
+			//{
+			//    if (P2 != null) P2.Bullet_cnt--;
+			//}
+			gameObject.SetActive(false);
+        }
+        else if (col.gameObject.tag == "Boss_Gard")
+        {
+            GameObject effect = Obj_Storage.Storage_Data.Effects[11].Active_Obj();
+            ParticleSystem particle = effect.GetComponent<ParticleSystem>();
+            effect.transform.position = gameObject.transform.position;
+            particle.Play();
+            gameObject.SetActive(false);
+        }
+    }
 }
