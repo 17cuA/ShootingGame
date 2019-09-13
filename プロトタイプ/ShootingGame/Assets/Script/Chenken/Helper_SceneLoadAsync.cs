8d1daccba141ceffd63ebc299429c6f31ba1e202
741PC-22\student <cu.common@gmail.com>
2019-09-13 10:23:17 +0900

    予告修正

---
+++ プロトタイプ/ShootingGame/Assets/Script/Chenken/Helper_SceneLoadAsync.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Helper_SceneLoadAsync : MonoBehaviour
+{
+	[SerializeField] private bool isAutoLoading = false;
+	public bool IsAutoLoading
+	{
+		get
+		{
+			return isAutoLoading;
+		}
+		set
+		{
+			isAutoLoading = value;
+		}
+	}
+
+	AsyncOperation async;   
+	[SerializeField] private string SceneName;
+	[SerializeField] private Slider loadingSlider;
+	[SerializeField] private Text loadingText; 
+	[SerializeField] private float loadingSpeed = 1;
+
+	private float targetValue;
+
+	private void Start()
+	{
+		loadingSlider.value = 0.0f;
+	}
+
+	private void Update()
+	{
+		if(isAutoLoading)
+		{
+			StartCoroutine("LoadScene");
+			isAutoLoading = false;
+		}
+
+		if (async == null) return;
+
+		targetValue = async.progress;
+
+
+		if (async.progress >= 0.9f)
+		{
+			targetValue = 1.0f;
+		}
+
+		if (targetValue != loadingSlider.value)
+		{
+			loadingSlider.value = Mathf.Lerp(loadingSlider.value, targetValue, Time.deltaTime * loadingSpeed);
+
+			if (Mathf.Abs(loadingSlider.value - targetValue) < 0.01f)
+			{
+
+				loadingSlider.value = targetValue;
+			}
+		}
+
+		loadingText.text = ((int)(loadingSlider.value * 100)).ToString() + "%";
+
+		if ((int)(loadingSlider.value * 100) == 100)
+			async.allowSceneActivation = true;
+	}
+
+	private IEnumerator LoadScene()
+	{
+		async = SceneManager.LoadSceneAsync(SceneName);
+
+		//async.allowSceneActivation = false;
+
+		print("Loading:" + async);
+
+		yield return async;
+	}
+}
+
+
