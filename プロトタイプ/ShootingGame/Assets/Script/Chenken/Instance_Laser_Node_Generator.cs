8dbef1fadca7aac8a2615ed9b48cdee7534f7b7d
chenkenZzz <d512668247@gmail.com>
2019-09-14 21:32:32 +0900

    修正その２

--- プロトタイプ/ShootingGame/Assets/Script/Chenken/Instance_Laser_Node_Generator.cs
+++ プロトタイプ/ShootingGame/Assets/Script/Chenken/Instance_Laser_Node_Generator.cs
@@ -103,9 +103,10 @@ public class Instance_Laser_Node_Generator : MonoBehaviour
              {
                 if(!Obj_Storage.Storage_Data.Laser_Line.Get_Obj()[i].gameObject.activeSelf)
                 { 
-                      this.nodes.Remove(Obj_Storage.Storage_Data.Laser_Line.Get_Obj()[i]);
-                      Destroy(Obj_Storage.Storage_Data.Laser_Line.Get_Obj()[i]);
-                      Obj_Storage.Storage_Data.Laser_Line.Get_Obj().RemoveAt(i);
+                    if(this.nodes.Contains(Obj_Storage.Storage_Data.Laser_Line.Get_Obj()[i]))
+                        this.nodes.Remove(Obj_Storage.Storage_Data.Laser_Line.Get_Obj()[i]);
+                    Destroy(Obj_Storage.Storage_Data.Laser_Line.Get_Obj()[i]);
+                    Obj_Storage.Storage_Data.Laser_Line.Get_Obj().RemoveAt(i);
                 }
              }
         }
