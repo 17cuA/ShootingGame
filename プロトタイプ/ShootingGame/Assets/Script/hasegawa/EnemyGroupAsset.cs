a2080e2ed90501b6c445fb8f0ef400fec9981466
741PC-36\student <cu.common@gmail.com>
2019-10-08 10:43:52 +0900

    ScriptableObjectを作ってみたよ

---
+++ プロトタイプ/ShootingGame/Assets/Script/hasegawa/EnemyGroupAsset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "EnemyGroupAsset", menuName = "ScriptableObject/EnemyGroup", order = 0)]
+public class EnemyGroupAsset : ScriptableObject
+{
+	public List<EnemyCreate.EnemyGroup> enemyGroups = new List<EnemyCreate.EnemyGroup>();
+}
+
+namespace ScriptableCreater
+{
+	public class CreaterEnemyGroupAsset
+	{
+		[MenuItem("ScriptableObject/EnemyGroup")]
+		public static void Create()
+		{
+			EnemyGroupAsset enemyGroupAsset = ScriptableObject.CreateInstance<EnemyGroupAsset>();
+			AssetDatabase.CreateAsset(enemyGroupAsset, "Assets/EnemyGroupAsset.asset");
+		}
+	}
+}
