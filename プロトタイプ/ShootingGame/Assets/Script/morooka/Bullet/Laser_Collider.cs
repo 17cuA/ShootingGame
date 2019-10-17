2a9a263ac6aef537928e5ddd7c7d47df8b7687f4
741PC-37\student <cu.common@gmail.com>
2019-10-17 15:09:31 +0900

    2ボス中

--- プロトタイプ/ShootingGame/Assets/Script/morooka/Bullet/Laser_Collider.cs
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/Bullet/Laser_Collider.cs
@@ -11,15 +11,47 @@ using UnityEngine;
 
 public class Laser_Collider : MonoBehaviour
 {
+	/// <summary>
+	///  ボスの種類
+	/// </summary>
+	public enum BOSS_TYPE
+	{
+		eBOSS_ONE,			// 一ボス
+		eBOSS_TWO,			// 二ボス
+	}
+
+	public BOSS_TYPE bType;
+
 	protected void OnTriggerEnter(Collider col)
 	{
-		// プレイヤーに衝突したとき
-		if (col.gameObject.tag == "Player")
+		switch (bType)
 		{
-			GameObject effect = Obj_Storage.Storage_Data.Effects[11].Active_Obj();
-			ParticleSystem particle = effect.GetComponent<ParticleSystem>();
-			effect.transform.position = gameObject.transform.position;
-			particle.Play();
+			case BOSS_TYPE.eBOSS_ONE:
+				// プレイヤーに衝突したとき
+				if (col.gameObject.tag == "Player")
+				{
+					GameObject effect = Obj_Storage.Storage_Data.Effects[11].Active_Obj();
+					ParticleSystem particle = effect.GetComponent<ParticleSystem>();
+					effect.transform.position = gameObject.transform.position;
+					particle.Play();
+				}
+				break;
+			case BOSS_TYPE.eBOSS_TWO:
+				if (col.gameObject.tag == "Player")
+				{
+					GameObject effect = Obj_Storage.Storage_Data.Effects[11].Active_Obj();
+					ParticleSystem particle = effect.GetComponent<ParticleSystem>();
+					effect.transform.position = gameObject.transform.position;
+					particle.Play();
+				}
+				else if (col.gameObject != transform.parent && col.gameObject.layer != 14 && col.gameObject.tag != "Option")
+				{
+					var temp = transform.parent;
+					temp.GetComponent<Two_Boss_Laser>().Scraps.Add(gameObject);
+				}
+				break;
+			default:
+				break;
 		}
 	}
 }
