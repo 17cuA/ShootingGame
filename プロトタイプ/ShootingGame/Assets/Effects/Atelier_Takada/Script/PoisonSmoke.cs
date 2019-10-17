ca909245040894fb5b30b39a6f339580729b5247
741PC-39\student <cu.common@gmail.com>
2019-10-17 15:01:13 +0900

    作業

---
+++ プロトタイプ/ShootingGame/Assets/Effects/Atelier_Takada/Script/PoisonSmoke.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonSmoke : character_status
+{
+
+
+    void Start()
+    {
+        
+    }
+
+
+
+    void Update()
+    {
+
+
+		if (transform.position.y <= -30.0f)
+		{
+			Destroy(this.gameObject);
+		}
+
+	}
+}
---
+++ プロトタイプ/ShootingGame/Assets/Effects/Atelier_Takada/Script/test/SponEff.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SponEff : MonoBehaviour
+{
+	public GameObject obj;
+	public float time;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		time = 0.0f;
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		time += Time.deltaTime;
+
+
+		if (time > 1.0f)
+		{
+			Vector3 vec3 = new Vector3(
+					Random.Range(-0.5f, 0.5f),
+					Random.Range(-0.5f, 0.5f)-1.0f,
+					0.0f);
+
+			Instantiate(obj, transform.position + vec3, transform.rotation);
+			time = 0.0f;
+		}
+
+	}
+}
