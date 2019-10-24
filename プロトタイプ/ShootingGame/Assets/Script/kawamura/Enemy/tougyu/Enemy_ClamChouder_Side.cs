37b7287f764edfb444cb47d6f6381f46d17c5dd8
741PC-16\student <cu.common@gmail.com>
2019-10-24 11:40:08 +0900

    seve

--- プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy/tougyu/Enemy_ClamChouder_Side.cs
+++ プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy/tougyu/Enemy_ClamChouder_Side.cs
@@ -1,17 +1,15 @@
-﻿//作成者：川村良太
-//画面奥からくる敵　敵の出現方法を変えるために新しく作ったやつ
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using StorageReference;
 
-public class Enemy_ClamChowder_Back : character_status
+public class Enemy_ClamChouder_Side : character_status
 {
 	public enum State
 	{
-		WaveUp,
-		WaveDown,
+		WaveOnlyUp,
+		WaveOnlyDown,
+		Straight,
 	}
 	public State eState;
 
@@ -63,18 +61,18 @@ public class Enemy_ClamChowder_Back : character_status
 	[Header("死亡時の弾発射の角度範囲()")]
 	public float diedAttack_RotaValue;
 
-
 	public bool isAddSpeedY = false;    //Yスピードを増加させるかどうか
 	public bool isSubSpeedY = false;    //Yスピードを減少させるかどうか
 
 	public bool once = true;            //updateで一回だけ呼び出す処理用
 	public bool isWave = false;         //奥からくる敵を上下移動に変える用
+	public bool isStraight = false;     //直進かどうか
 	public bool isOnlyWave;             //上下移動のみか（左へ進みながら）
 	public bool haveItem = false;
 
 	public bool isSlerp = false;
 	public bool isNoSlerp = false;
-	public bool utsuttemasuyo = false;
+	bool isSonicPlay = false;
 	bool isWaveStart = false;
 	public bool Died_Attack = false;
 	public bool isFromBack = false;             //奥からくるやつ用
@@ -84,6 +82,8 @@ public class Enemy_ClamChowder_Back : character_status
 
 	private void Awake()
 	{
+		//sonicBoom.Stop();
+		isSonicPlay = false;
 		defaultPos = transform.localPosition;
 
 		if (gameObject.GetComponent<DropItem>())
@@ -91,25 +91,7 @@ public class Enemy_ClamChowder_Back : character_status
 			DropItem dItem = gameObject.GetComponent<DropItem>();
 			haveItem = true;
 		}
-		switch (eState)
-		{
-			case State.WaveUp:
-				isFromBack = true;
-				isBehind = false;
-				endMarker = new Vector3(12.0f, transform.position.y, 0);
-				distance_two = Vector3.Distance(startMarker, endMarker);
-				break;
-
-			case State.WaveDown:
-				isFromBack = true;
-				isBehind = false;
-				endMarker = new Vector3(12.0f, transform.position.y, 0);
-				distance_two = Vector3.Distance(startMarker, endMarker);
-				break;
-		}
 	}
-
-
 	new void Start()
     {
 		//startMarker = new Vector3(12.0f, transform.position.y, 40.0f);
@@ -119,8 +101,6 @@ public class Enemy_ClamChowder_Back : character_status
 																//childObj_Shot = transform.GetChild(1).gameObject;
 		childObj_Angle = transform.GetChild(1).gameObject;
 		//childCnt = transform.childCount;
-		fd = childObj_Angle.GetComponent<Find_Angle>();
-
 		if (transform.parent)
 		{
 			parentObj = transform.parent.gameObject;
@@ -138,136 +118,103 @@ public class Enemy_ClamChowder_Back : character_status
 		//posZ = -5.0f;
 		//defPosX = (13.0f - transform.position.x) / 120.0f;         //13.0fはとりあえず敵が右へ向かう限界の座標
 		startTime = 0.0f;
+
 		HP_Setting();
 		base.Start();
+    }
 
-	}
-
-	new void Update()
-	{
+    // Update is called once per frame
+    new void Update()
+    {
 		if (once)
 		{
 			clamSpawner_Script = parentObj.GetComponent<Enemy_ClamChowder_Spawner>();
-			//transform.localPosition = defaultPos;
+
 			//状態によって値を変える
 			switch (eState)
 			{
-
-				//画面左から右へ、後ろからきて上下移動は上からし始める
-				case State.WaveUp:
-					isOnlyWave = false;
-					isBehind = false;
+				//画面右からきて上下移動は上からし始める
+				case State.WaveOnlyUp:
+					isWaveStart = true;
 					if (defaultSpeedY < 0)
 					{
 						defaultSpeedY *= -1;
 					}
-					isSubSpeedY = true;
-					isAddSpeedY = false;
-					speedX = 18;
-					speedZ_Value = 40;
-					//transform.position = new Vector3(transform.position.x, transform.position.y, 40.0f);
-					isWave = false;
+					//speedY = defaultSpeedY;
+					speedY = 0;
+					amplitude = 0.1f;
+					speedX = 7.5f;
+					speedZ_Value = 0;
+					isStraight = false;
+					isOnlyWave = true;
+					isAddSpeedY = true;
 					HSV_Change();
 					break;
 
-				//画面左から右へ、後ろからきて上下移動は下からし始める
-				case State.WaveDown:
-					isOnlyWave = false;
-					isBehind = false;
+				//画面右からきて上下移動は下からし始める
+				case State.WaveOnlyDown:
+					isWaveStart = true;
 					if (defaultSpeedY > 0)
 					{
 						defaultSpeedY *= -1;
 					}
-					isAddSpeedY = true;
-					isSubSpeedY = false;
-					speedX = 18;
-					speedZ_Value = 40;
-					//transform.position = new Vector3(transform.position.x, transform.position.y, 40.0f);
-					isWave = false;
+					speedY = 0;
+					amplitude = -0.1f;
+					speedX = 7.5f;
+					speedZ_Value = 0;
+					isOnlyWave = true;
+					isStraight = false;
+					isSubSpeedY = true;
+					HSV_Change();
+					break;
+
+				//直進
+				case State.Straight:
+					transform.position = new Vector3(transform.position.x, transform.position.y, 0.0f);
+					isStraight = true;
+					speedX = 7.5f;
+					amplitude = 0;
 					HSV_Change();
 					break;
 			}
 			once = false;
 		}
 
-		if (!isWave)
+		//直進状態
+		if (eState == State.Straight)
 		{
-			if (isSlerp)
-			{
-				present_Location = (Time.time * testSpeed) / distance_two;
-				transform.position = Vector3.Slerp(startMarker, endMarker, startTime);
-				startTime += slaep_IncValue;
-				if (startTime > 1)
-				{
-					startTime = 1;
-				}
-				//startTime++;
-				HSV_Change();
-
-				if (transform.position == endMarker)
-				{
-					isWave = true;
-					transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-
-					//前からなら
-					if (!isBehind)
-					{
-						speedX = 7.5f;
-					}
-					//後ろからなら
-					else if (isBehind)
-					{
-						speedX = -7.5f;
-					}
-					speedY = defaultSpeedY;
-
-				}
-			}
-			else if (isNoSlerp)
-			{
-				velocity = gameObject.transform.rotation * new Vector3(speedX, 0, -speedZ);
-				gameObject.transform.position += velocity * Time.deltaTime;
-
-				//前からなら
-				if (!isBehind)
-				{
-					if (transform.position.z < 0)
-					{
-						transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-						speedX = 7.5f;
-						speedY = defaultSpeedY;
-						isWave = true;
-					}
-
-					if (transform.position.x >= 12)
-					{
-						isSlerp = true;
-						startMarker = transform.position;
-						//sonicBoom.Play();
-					}
-
-					if (transform.position.x > 7)
-					{
-						//明るさを変える関数
-						HSV_Change();
-
-					}
-				}
-			}
-		}
-		else if (isWave)
-		{
-			speedX = 7.5f;
-			//sin =posY + Mathf.Sin(Time.time*5);
-
-			SpeedY_Check();
-			SpeedY_Calculation();
-
-			//this.transform.position = new Vector3(transform.position.x, sin, 0);
-			//transform.position = new Vector3(transform.position.x, Mathf.Sin(Time.frameCount * 0.1f), transform.position.z);
-			velocity = gameObject.transform.rotation * new Vector3(-speedX, speedY, 0);
+			velocity = gameObject.transform.rotation * new Vector3(-speedX, 0, 0);
 			gameObject.transform.position += velocity * Time.deltaTime;
 		}
+		//上下移動状態
+		else if (eState == State.WaveOnlyUp || eState == State.WaveOnlyDown)
+		{
+			if (transform.position.x < 20 && transform.position.z == 0)
+			{
+				if (!isWaveStart)
+				{
+					speedY = defaultSpeedY;
+					isWaveStart = true;
+				}
+			}
+
+			if (isWaveStart)
+			{
+				SpeedY_Check();
+				SpeedY_Calculation();
+
+			}
+			else
+			{
+				speedY = 0;
+			}
+			//transform.position = new Vector3(transform.position.x, startPosY + Mathf.Sin(Time.frameCount * amplitude), transform.position.z);
+			velocity = gameObject.transform.rotation * new Vector3(-speedX, speedY, 0);
+
+			//velocity = gameObject.transform.rotation * new Vector3(-speedX, 0, 0);
+			gameObject.transform.position += velocity * Time.deltaTime;
+		}
+
 		if (hp < 1)
 		{
 			if (haveItem)
@@ -288,33 +235,14 @@ public class Enemy_ClamChowder_Back : character_status
 				if (parentObj.name != "TemporaryParent")
 				{
 					//群を管理している親の残っている敵カウントマイナス
-					//groupManage.remainingEnemiesCnt--;
-					clamSpawner_Script.remainingEnemiesCnt--;
+					groupManage.remainingEnemiesCnt--;
 					//倒された敵のカウントプラス
-					//groupManage.defeatedEnemyCnt++;
-					clamSpawner_Script.defeatedEnemyCnt++;
+					groupManage.defeatedEnemyCnt++;
 					//群に残っている敵がいなくなったとき
-					//if (groupManage.remainingEnemiesCnt == 0)
-					//{
-					//	//倒されずに画面外に出た敵がいなかったとき(すべての敵が倒されたとき)
-					//	if (groupManage.notDefeatedEnemyCnt == 0 && groupManage.isItemDrop)
-					//	{
-					//		//アイテム生成
-					//		//Instantiate(item, this.transform.position, transform.rotation);
-					//		Object_Instantiation.Object_Reboot(Game_Master.OBJECT_NAME.ePOWERUP_ITEM, this.transform.position, transform.rotation);
-					//	}
-					//	//一体でも倒されていないのがいたら
-					//	else
-					//	{
-					//		//なにもしない
-					//	}
-					//	groupManage.itemPos = transform.position;
-					//	groupManage.itemTransform = this.transform;
-					//}
-					if (clamSpawner_Script.remainingEnemiesCnt == 0)
+					if (groupManage.remainingEnemiesCnt == 0)
 					{
 						//倒されずに画面外に出た敵がいなかったとき(すべての敵が倒されたとき)
-						if (clamSpawner_Script.notDefeatedEnemyCnt == 0 && clamSpawner_Script.isItemDrop)
+						if (groupManage.notDefeatedEnemyCnt == 0 && groupManage.isItemDrop)
 						{
 							//アイテム生成
 							//Instantiate(item, this.transform.position, transform.rotation);
@@ -325,6 +253,8 @@ public class Enemy_ClamChowder_Back : character_status
 						{
 							//なにもしない
 						}
+						groupManage.itemPos = transform.position;
+						groupManage.itemTransform = this.transform;
 					}
 				}
 			}
@@ -332,10 +262,13 @@ public class Enemy_ClamChowder_Back : character_status
 			//Reset_Status();
 			Died_Process();
 		}
+
+
 		base.Update();
+    }
+	//-------------ここから関数------------------
 
-	}
-
+	//Yスピードを見てYスピードを増加させるか減少させるかを決める
 	void SpeedY_Check()
 	{
 		if (defaultSpeedY >= 0)
@@ -402,10 +335,9 @@ public class Enemy_ClamChowder_Back : character_status
 
 	private void OnTriggerExit(Collider col)
 	{
-		if (col.gameObject.name == "BattleshipType_Enemy(Clone)"|| col.gameObject.name == "BattleshipType_Enemy")
+		if (col.gameObject.name == "BattleshipType_Enemy(Clone)" || col.gameObject.name == "BattleshipType_Enemy")
 		{
 			hp = 0;
 		}
 	}
-
 }
