06144da3f0e70c385c68a553094dc03d81d616c4
741PC-14\student <cu.common@gmail.com>
2019-10-24 14:56:10 +0900

    煙と調整

--- プロトタイプ/ShootingGame/Assets/Effects/Atelier_Takada/Script/PoisonSmoke.cs
+++ プロトタイプ/ShootingGame/Assets/Effects/Atelier_Takada/Script/PoisonSmoke.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class PoisonSmoke : character_status
 {
+	public float time;
 
-
-    void Start()
+	void Start()
     {
-        
-    }
+		time = 0.0f;
+
+	}
 
 
 
     void Update()
     {
-
-
-		if (transform.position.y <= -30.0f)
+		if(time < 0.5f)
 		{
-			Destroy(this.gameObject);
+			time += Time.deltaTime;
+
+			if (time > 0.5f)
+			{
+				GetComponent<SphereCollider>().enabled = true;
+			}
 		}
 
 	}
--- プロトタイプ/ShootingGame/Assets/Effects/Atelier_Takada/Script/test/SponEff.cs
+++ プロトタイプ/ShootingGame/Assets/Effects/Atelier_Takada/Script/test/SponEff.cs
@@ -6,12 +6,13 @@ public class SponEff : MonoBehaviour
 {
 	public GameObject obj;
 	public float time;
+	public float interval;
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		time = 0.0f;
-
+		interval = Random.Range(0.1f, 1.0f);
 	}
 
 	// Update is called once per frame
@@ -20,7 +21,7 @@ public class SponEff : MonoBehaviour
 		time += Time.deltaTime;
 
 
-		if (time > 1.0f)
+		if (time > interval)
 		{
 			Vector3 vec3 = new Vector3(
 					Random.Range(-0.5f, 0.5f),
@@ -28,6 +29,7 @@ public class SponEff : MonoBehaviour
 					0.0f);
 
 			Instantiate(obj, transform.position + vec3, transform.rotation);
+			interval = Random.Range(0.1f, 1.0f);
 			time = 0.0f;
 		}
 
