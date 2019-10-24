8894e286c2afa10123d5e8a66dd1273da4af422a
741PC-16\student <cu.common@gmail.com>
2019-10-24 12:10:58 +0900

    ラスボスのバキュラのアイテム一個だけ

--- プロトタイプ/ShootingGame/Assets/Script/morooka/BossScrits/TwoBoss_Set/Smasher.cs
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/BossScrits/TwoBoss_Set/Smasher.cs
@@ -5,12 +5,12 @@ using StorageReference;
 
 public class Smasher : MonoBehaviour
 {
-	private void OnTriggerEnter(Collider other)
-	{
-		if(other.name == "Bacula")
-		{
-			Object_Instantiation.Object_Reboot(Game_Master.OBJECT_NAME.ePOWERUP_ITEM, other.transform.position, Quaternion.identity);
-			GetComponent<Smasher>().enabled = false;
-		}
-	}
+	//private void OnTriggerEnter(Collider other)
+	//{
+	//	if(other.name == "Bacula")
+	//	{
+	//		Object_Instantiation.Object_Reboot(Game_Master.OBJECT_NAME.ePOWERUP_ITEM, other.transform.position, Quaternion.identity);
+	//		GetComponent<Smasher>().enabled = false;
+	//	}
+	//}
 }
