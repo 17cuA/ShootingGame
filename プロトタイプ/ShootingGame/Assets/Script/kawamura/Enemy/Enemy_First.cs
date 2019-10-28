aa66a016677961646c69a57ced15893c96e19d76
741PC-16\student <cu.common@gmail.com>
2019-10-28 15:02:33 +0900

    円盤の初期の頃の移動が戻った

--- プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy/Enemy_First.cs
+++ プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy/Enemy_First.cs
@@ -10,50 +10,39 @@ public class Enemy_First : character_status
 {
 	public enum State
 	{
-		TurnUp,         //上に曲がる
-		TurnDown,       //下に曲がる
-		defaultStraight,
-		Straight,       //生成された時(曲がらない)
+		TurnUp,				//上に曲がる
+		TurnDown,			//下に曲がる
+		defaultStraight,	//画面奥からくる直進
+		Straight,			//排出とかで生成された時(曲がらない)
 	}
 
-	public State eState;
-	GameObject item;
-	public GameObject parentObj;
-	GameObject childObj;
-	private GameObject Bullet;  //弾のプレハブ、リソースフォルダに入っている物を名前から取得。
+	public State eState;			//移動状態
+	public GameObject parentObj;	//親取得用
+	GameObject childObj;			//子供取得用
 
 	Vector3 velocity;
 	public Vector3 defaultPos_Local;
     public Vector3 defaultPos_PlusZ;
-	public Quaternion diedAttackRota;
-	public Transform diedAttack_Transform;
+	public Quaternion diedAttackRota;			//死んだ時に出す弾の角度範囲
+	public Transform diedAttack_Transform;		//死んだ時に弾を出す場合の位置
 
-	public EnemyGroupManage groupManage;
-	Find_Angle fd;
-	Enemy_BurstShot ebs;
-	//Renderer renderer;
+	public EnemyGroupManage groupManage;		//群管理スクリプト取得用
+	Find_Angle fd;								//プレイヤーの方向を向くスクリプト取得用（死亡時攻撃に使う）
 
-	public float timeCnt = 0;                   //回転の度合い（0～59）で周期
-	public float circleSpeed = 10.0f;             //移動速度
-	public float radius = 1.0f;             //回転する円の大きさ
 	[Header("Zポジションの移動開始位置")]
 	public float ZMovePos;
-	float _y;
-	float _z;
 
-	public float defaultPosY_World;
-	public float defaultPosY_Local;
-	public float endPosY_Local;
-	public float localPosY;
+	public float defaultPosY_World;		//最初のワールドY
+	public float defaultPosY_Local;		//最初のローカルY
+	public float endPosY_Local;			//手前に来た時のYの位置
+	public float localPosY;				
 	//float frame = 0;
-	float straightFrame;
-	float straightFrame_Default;
 	public float speedX;
-	public float speedX_Straight;
+	public float speedX_Straight;	//排出された時のスピード
 	public float speedY;
     public float speedZ;
     public float speedZ_Value;
-	public float diedAttack_RotaZ;
+	public float diedAttack_RotaZ;			//死亡時の弾の角度範囲
 	[Header("死亡時の弾発射の角度範囲()")]
 	public float diedAttack_RotaValue;
 	bool once = true;
@@ -65,11 +54,7 @@ public class Enemy_First : character_status
 
 	private void Awake()
 	{
-		//renderer = childObj.GetComponent<Renderer>();
-		Bullet = Resources.Load("Bullet/Enemy_Bullet") as GameObject;
 
-		straightFrame_Default = 300;
-		straightFrame = straightFrame_Default;
 		defaultPos_Local = transform.localPosition;
         defaultPos_PlusZ = defaultPos_Local + new Vector3(0, 0, 40);
 		if (gameObject.GetComponent<DropItem>())
@@ -81,11 +66,8 @@ public class Enemy_First : character_status
 
 	new void Start()
 	{
-		item = Resources.Load("Item/Item_Test") as GameObject;
 		childObj = transform.GetChild(0).gameObject;
 		fd = childObj.GetComponent<Find_Angle>();
-		ebs = childObj.GetComponent<Enemy_BurstShot>();
-		//renderer = gameObject.GetComponent<Renderer>();
 
 		//speedX = 5.0f;
 		speedY = 5.0f;
@@ -156,21 +138,7 @@ public class Enemy_First : character_status
 				//transform.localPosition = defaultPos_Local;
 
 				speedX = 5;
-     //               if (transform.position.y > 0)
-					//{
-					//	//transform.localPosition = defaultPos_Local;
-					//	speedX = 5;
-     //                   speedY = 5;
-					//	eState = State.TurnDown;
-					//}
-					//else
-					//{
-					//	//transform.localPosition = defaultPos_Local;
-					//	speedX = 5;
-     //                   speedY = 5;
-					//	eState = State.TurnUp;
-					//}
-				//}
+				speedY = 5;
 			}
 			once = false;
 		}
@@ -225,7 +193,6 @@ public class Enemy_First : character_status
 			//Reset_Status();
 			//isDead = true;
 			//isTurn = false;
-			//straightFrame = straightFrame_Default;
 			//frame = 0;
 			Enemy_Reset();
 			Died_Process();
@@ -241,62 +208,15 @@ public class Enemy_First : character_status
 	{
 		switch (eState)
 		{
+			//画面奥からくる直進
 			case State.defaultStraight:
+				MoveStraight();
+				break;
+
+			case State.TurnUp:
 				if (!isTurn)
 				{
-					//straightFrame--;
-					velocity = gameObject.transform.rotation * new Vector3(-speedX, 0, -speedZ);
-					gameObject.transform.position += velocity * Time.deltaTime;
-					localPosY = transform.localPosition.z * (-endPosY_Local / 20.0f) + endPosY_Local;
-					gameObject.transform.localPosition = new Vector3(transform.localPosition.x, defaultPosY_Local + localPosY, transform.localPosition.z);
-					//HSV_Change();
-
-					if (transform.position.z < 0)
-					{
-						transform.position = new Vector3(transform.position.x, transform.position.y, 0.0f);
-						speedZ = 0;
-						speedZ_Value = 0;
-					}
-
-					if (transform.localPosition.x <= ZMovePos)
-					{
-						speedZ = speedZ_Value;
-					}
-                    //if (transform.localPosition.x <= -32)
-                    if (transform.localPosition.x <= -40.5)
-                    {
-                        //frame += Time.deltaTime;
-                        //if (frame > 3)
-                        //{
-                        //	isTurn = true;
-                        //}
-                        speedX = 7.5f;
-						//isTurn = true;
-					}
-
-                    //else if (transform.localPosition.x < -21)
-                    else if (transform.localPosition.x < -25.5)
-                    {
-                        speedX -= 0.48f;
-						if (speedX < 7.5f)
-						{
-							speedX = 7.5f;
-						}
-
-					}
-                    //else if (transform.localPosition.x < -9)
-                    else if (transform.localPosition.x < ZMovePos)
-                    {
-						speedX += 0.24f;
-					}
-					//if (transform.position.x < 9)
-					//{
-					//	frame += Time.deltaTime;
-					//	if (frame > 3)
-					//	{
-					//		isTurn = true;
-					//	}
-					//}
+					MoveStraight();
 				}
 				else if (isTurn)
 				{
@@ -317,151 +237,29 @@ public class Enemy_First : character_status
 				}
 				break;
 
-			//case State.TurnUp:
-			//	if (!isTurn)
-			//	{
-			//		//straightFrame--;
-			//		velocity = gameObject.transform.rotation * new Vector3(-speedX, 0, -speedZ);
-			//		gameObject.transform.position += velocity * Time.deltaTime;
-			//		localPosY = transform.localPosition.z * (-endPosY_Local / 20.0f) + endPosY_Local;
-			//		gameObject.transform.localPosition = new Vector3(transform.localPosition.x, defaultPosY_Local + localPosY, transform.localPosition.z);
-			//		//HSV_Change();
+			case State.TurnDown:
+				if (!isTurn)
+				{
+					MoveStraight();
+				}
+				else if (isTurn)
+				{
+					velocity = gameObject.transform.rotation * new Vector3(speedX, -speedY, 0);
+					gameObject.transform.position += velocity * Time.deltaTime;
 
-			//		if (transform.position.z < 0)
-			//		{
-			//			transform.position = new Vector3(transform.position.x, transform.position.y, 0.0f);
-			//			speedZ = 0;
-			//			speedZ_Value = 0;
-			//		}
+					speedX -= 0.12f;
 
-			//		if (transform.localPosition.x <= ZMovePos)
-			//		{
-			//			speedZ = speedZ_Value;
-			//		}
-			//		if (transform.localPosition.x <= -32)
-			//		{
-			//			//frame += Time.deltaTime;
-			//			//if (frame > 3)
-			//			//{
-			//			//	isTurn = true;
-			//			//}
-			//			speedX = 5;
-			//			//isTurn = true;
-			//		}
-
-			//		else if (transform.localPosition.x < -21)
-			//		{
-			//			speedX -= 0.36f;
-			//			if (speedX < 5)
-			//			{
-			//				speedX = 5;
-			//			}
-
-			//		}
-			//		else if (transform.localPosition.x < -9)
-			//		{
-			//			speedX += 0.12f;
-			//		}
-			//		//if (transform.position.x < 9)
-			//		//{
-			//		//	frame += Time.deltaTime;
-			//		//	if (frame > 3)
-			//		//	{
-			//		//		isTurn = true;
-			//		//	}
-			//		//}
-			//	}
-			//	else if (isTurn)
-			//	{
-			//		velocity = gameObject.transform.rotation * new Vector3(speedX, speedY, 0);
-			//		gameObject.transform.position += velocity * Time.deltaTime;
-
-			//		speedX -= 0.12f;
-
-			//                 if (transform.position.y > 0.5)
-			//                 {
-			//                     speedX -= 0.36f;
-			//                     speedY += 0.36f;
-			//                 }
-			//                 if (speedX < -12.0f)
-			//                 {
-			//                     speedX = -12.0f;
-			//                 }
-			//             }
-			//             break;
-
-			//case State.TurnDown:
-			//	if (!isTurn)
-			//	{
-			//		velocity = gameObject.transform.rotation * new Vector3(-speedX, 0, -speedZ);
-			//		gameObject.transform.position += velocity * Time.deltaTime;
-			//		localPosY = transform.localPosition.z * (-endPosY_Local / 20.0f) + endPosY_Local;
-			//		gameObject.transform.localPosition = new Vector3(transform.localPosition.x, defaultPosY_Local + localPosY, transform.localPosition.z);
-
-			//		//HSV_Change();
-			//		if (transform.position.z < 0)
-			//		{
-			//			transform.position = new Vector3(transform.position.x, transform.position.y, 0.0f);
-			//			speedZ = 0;
-			//			speedZ_Value = 0;
-			//		}
-
-			//		if (transform.localPosition.x <= ZMovePos)
-			//		{
-			//			speedZ = speedZ_Value;
-			//		}
-
-			//		if (transform.localPosition.x <= -32)
-			//		{
-			//			//frame += Time.deltaTime;
-			//			//if (frame > 3)
-			//			//{
-			//			//	isTurn = true;
-			//			//}
-			//			speedX = 5;
-			//			//isTurn = true;
-			//		}
-			//		else if (transform.localPosition.x < -21)
-			//		{
-			//			speedX -= 0.36f;
-			//			if (speedX < 5)
-			//			{
-			//				speedX = 5;
-			//			}
-			//		}
-
-			//		else if (transform.localPosition.x < -9)
-			//		{
-			//			speedX += 0.12f;
-			//		}
-
-			//		//if (transform.position.x < 9)
-			//		//{
-			//		//	frame += Time.deltaTime;
-			//		//}
-			//		//if (frame > 3)
-			//		//{
-			//		//	isTurn = true;
-			//		//}
-			//	}
-			//	else if (isTurn)
-			//	{
-			//		velocity = gameObject.transform.rotation * new Vector3(speedX, -speedY, 0);
-			//		gameObject.transform.position += velocity * Time.deltaTime;
-
-			//		speedX -= 0.12f;
-
-   //                 if (transform.position.y < -0.5)
-   //                 {
-   //                     speedX -= 0.36f;
-   //                     speedY += 0.36f;
-   //                 }
-			//		if (speedX < -12.0f)
-			//		{
-			//			speedX = -12.0f;
-			//		}
-			//	}
-			//	break;
+					if (transform.position.y < -0.5)
+					{
+						speedX -= 0.36f;
+						speedY += 0.36f;
+					}
+					if (speedX < -12.0f)
+					{
+						speedX = -12.0f;
+					}
+				}
+				break;
 
 			case State.Straight:
 				speedX = speedX_Straight;
@@ -470,6 +268,61 @@ public class Enemy_First : character_status
 
 				break;
 		}
+	}
+
+	//直進移動関数
+	void MoveStraight()
+	{
+		velocity = gameObject.transform.rotation * new Vector3(-speedX, 0, -speedZ);
+		gameObject.transform.position += velocity * Time.deltaTime;
+		//見た目上のY座標を常に一緒になるように調整
+		localPosY = transform.localPosition.z * (-endPosY_Local / 20.0f) + endPosY_Local;
+		//座標を代入
+		gameObject.transform.localPosition = new Vector3(transform.localPosition.x, defaultPosY_Local + localPosY, transform.localPosition.z);
+
+		//手前に来たら
+		if (transform.position.z < 0)
+		{
+			//Zスピード0にしたりする
+			transform.position = new Vector3(transform.position.x, transform.position.y, 0.0f);
+			speedZ = 0;
+			speedZ_Value = 0;
+		}
+
+		//手前に来始める位置に来たらZスピードを入れる
+		if (transform.localPosition.x <= ZMovePos)
+		{
+			speedZ = speedZ_Value;
+		}
+		//if (transform.localPosition.x <= -32)
+		if (transform.localPosition.x <= -42)
+		{
+			speedX = 7.5f;
+			if (eState == State.TurnDown || eState == State.TurnUp)
+			{
+				speedX = 5;
+			}
+			isTurn = true;
+			//isTurn = true;
+		}
+
+		//else if (transform.localPosition.x < -21)
+		//加速したスピードを減速
+		else if (transform.localPosition.x < -25.5)
+		{
+			speedX -= 0.48f;
+			if (speedX < 7.5f)
+			{
+				speedX = 7.5f;
+			}
+
+		}
+		//手前に来るときにXスピードを上げてスピード感を出す
+		else if (transform.localPosition.x < ZMovePos)
+		{
+			speedX += 0.24f;
+		}
+
 	}
 
 	//状態を変える(主に出現時に曲がらせたくないときに使うと思われます)
@@ -496,7 +349,6 @@ public class Enemy_First : character_status
 	void Enemy_Reset()
 	{
 		//frame = 0;
-		straightFrame = straightFrame_Default;
 		speedZ_Value = 75;
 		once = true;
 		isTurn = false;
