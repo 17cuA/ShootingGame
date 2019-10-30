d45948e6f943868309f9027e48d0980420e7aaed
741PC-14\student <cu.common@gmail.com>
2019-10-30 11:46:17 +0900

    クワガタ作ったんですが。（途中）

--- プロトタイプ/ShootingGame/Assets/Script/Yamazaki/Enemy_StagBeetle.cs
+++ プロトタイプ/ShootingGame/Assets/Script/Yamazaki/Enemy_StagBeetle.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 
 public class Enemy_StagBeetle : character_status
 {
-	
+	public enum State
+	{
+		NONE,
+		STRAIGHT,
+		SINMOVE,
+		SINMOVETARGET,
+		ATTACK,
+	}
+
+	public State eState = State.NONE;
 	   
 	public int sinAngleFrame;
 	public int sinAngleFrameMax = 30;
 	public float yRange = 8.0f;
 	public bool isUp;
 
-    // Start is called before the first frame update
-    private void Start()
+	public character_status playerObj; // 注視したいオブジェクトをInspectorから入れておく
+
+
+	// Start is called before the first frame update
+	private new void Start()
     {
+		eState = State.NONE;
+
 		isUp = true;
 
 		sinAngleFrame = 0;
@@ -23,23 +37,62 @@ public class Enemy_StagBeetle : character_status
 	}
 
     // Update is called once per frame
-    private void Update()
+    private new void Update()
     {
-		transform.position = new Vector3(0.0f, Mathf.Sin(((float)sinAngleFrame / (float)sinAngleFrameMax) * Mathf.PI * 2.0f) * yRange / 2.0f, 0.0f);
-		sinAngleFrame++;
+		// プレイヤー格納
+		TargetPlayer();
 
-		if (sinAngleFrame / sinAngleFrameMax > 0.25f && isUp && sinAngleFrame / sinAngleFrameMax < 0.5f)
+		switch (eState)
 		{
-			isUp = false;
+			case State.STRAIGHT:
+				transform.position += new Vector3(-speed / 60.0f, 0.0f, 0.0f);
+				if (Vector3.Distance(playerObj.transform.position, transform.position) <= 7.0f)
+				{
+					eState = State.SINMOVE;
+				}
+				break;
+
+			case State.SINMOVE:
+				transform.position = new Vector3(0.0f, Mathf.Sin(((float)sinAngleFrame / (float)sinAngleFrameMax) * Mathf.PI * 2.0f) * yRange / 2.0f, 0.0f);
+				sinAngleFrame++;
+				if (sinAngleFrame / sinAngleFrameMax >= 1.0f)
+				{
+					eState = State.SINMOVETARGET;
+				}
+				break;
+
+			case State.SINMOVETARGET:
+				transform.position = new Vector3(0.0f, Mathf.Sin(((float)sinAngleFrame / (float)sinAngleFrameMax) * Mathf.PI * 2.0f) * yRange / 2.0f, 0.0f);
+				sinAngleFrame++;
+				if (Mathf.Abs(playerObj.transform.position.y - transform.position.y) <= 1.0f)
+				{
+					eState = State.ATTACK;
+				}
+				break;
+
+			case State.ATTACK:
+				transform.position += new Vector3(-speed / 15.0f, 0.0f, 0.0f);
+				break;
+
+			default:
+				break;
 		}
-		else if (sinAngleFrame / sinAngleFrameMax > 0.75f && !isUp)
-		{
-			isUp = true;
-		}
-		else if (sinAngleFrame / sinAngleFrameMax >= 1.0f)
-		{
-			sinAngleFrame -= sinAngleFrameMax;
-		}
+
+		//transform.position = new Vector3(0.0f, Mathf.Sin(((float)sinAngleFrame / (float)sinAngleFrameMax) * Mathf.PI * 2.0f) * yRange / 2.0f, 0.0f);
+		//sinAngleFrame++;
+
+		//if (sinAngleFrame / sinAngleFrameMax > 0.25f && isUp && sinAngleFrame / sinAngleFrameMax < 0.5f)
+		//{
+		//	isUp = false;
+		//}
+		//else if (sinAngleFrame / sinAngleFrameMax > 0.75f && !isUp)
+		//{
+		//	isUp = true;
+		//}
+		//else if (sinAngleFrame / sinAngleFrameMax >= 1.0f)
+		//{
+		//	sinAngleFrame -= sinAngleFrameMax;
+		//}
 
 		HSV_Change();
 		if (hp < 1)
@@ -53,4 +106,42 @@ public class Enemy_StagBeetle : character_status
 		base.Update();
 	}
 
+	private void TargetPlayer()
+	{
+		if (playerObj == null)
+		{
+			if (Game_Master.Number_Of_People == Game_Master.PLAYER_NUM.eONE_PLAYER)
+			{
+				playerObj = Obj_Storage.Storage_Data.GetPlayer().GetComponent<character_status>();
+			}
+			else if (Game_Master.Number_Of_People == Game_Master.PLAYER_NUM.eTWO_PLAYER)
+			{
+				if (Vector3.Distance(Obj_Storage.Storage_Data.GetPlayer().transform.position, transform.position) < Vector3.Distance(Obj_Storage.Storage_Data.GetPlayer2().transform.position, transform.position))
+				{
+					playerObj = Obj_Storage.Storage_Data.GetPlayer().GetComponent<character_status>();
+				}
+				else
+				{
+					playerObj = Obj_Storage.Storage_Data.GetPlayer2().GetComponent<character_status>();
+				}
+			}
+		}
+		else if (playerObj != null)
+		{
+			// 片方のオブジェクトが消えてるとき
+			if (playerObj.Remaining == 0)
+			{
+				// 1P のとき
+				if (Obj_Storage.Storage_Data.GetPlayer().GetComponent<character_status>() == playerObj)
+				{
+					playerObj = playerObj = Obj_Storage.Storage_Data.GetPlayer2().GetComponent<character_status>();
+				}
+				// 2Pのとき
+				if (Obj_Storage.Storage_Data.GetPlayer2().GetComponent<character_status>() == playerObj)
+				{
+					playerObj = playerObj = Obj_Storage.Storage_Data.GetPlayer().GetComponent<character_status>();
+				}
+			}
+		}
+	}
 }
