3ce5cca2168582c007b8477e36d37c4fd59014be
741PC-37\student <cu.common@gmail.com>
2019-11-15 15:03:04 +0900

    okutopasu

---
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/Enemy/Parts_Collider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parts_Collider : MonoBehaviour
+{
+	private RaycastHit hit;
+	private RaycastHit HitObject { get { return hit; } }
+
+	[SerializeField, Tooltip("レイの長さ")]private float RayLength;
+	[SerializeField, Tooltip("レイン向き")]private Vector3 RayDirection;
+
+	public bool Is_HitRayCast()
+	{
+		return Physics.Raycast(transform.position, RayDirection, out hit, RayLength);
+	}
+}
