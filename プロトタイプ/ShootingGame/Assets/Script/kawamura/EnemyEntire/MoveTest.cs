f17d15d36525dce889798b5e4641ef5582c38711
741PC-17\student <uroncha.15.3@gmail.com>
2019-11-19 12:30:48 +0900

    いまいち

---
+++ プロトタイプ/ShootingGame/Assets/Script/kawamura/EnemyEntire/MoveTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTest : MonoBehaviour
+{
+	Vector3 velocity;
+	public float speed;
+
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		velocity = gameObject.transform.rotation * new Vector3(0, speed, 0);
+		gameObject.transform.position += velocity * Time.deltaTime;
+    }
+}
