d4b024b4b232bb69b359cd371988e94d004f6c10
741PC-17\student <uroncha.15.3@gmail.com>
2019-12-09 19:22:01 +0900

    ？

--- プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy2/RayCheck.cs
+++ プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy2/RayCheck.cs
@@ -4,39 +4,106 @@ using UnityEngine;
 
 public class RayCheck : MonoBehaviour
 {
+	public GameObject topObj;
+	Enemy_FollowGround_Ray topParent_Script;
+
+	public GameObject parentObj;
+	RayParent raytopParent_Script;
+
+	public GameObject angleObj;
+	AngleChange angleChange_Script;
+
+
 	RaycastHit hit; //ヒットしたオブジェクト情報
 
+	public float rayDelayMax;
+	public float rayDelayCnt;
+
 	public Vector3 groundAngle;
+	public Vector3 saveAngle;
+	public float angleZ;
+	string myName;
+	public bool isVertical = false;     //縦
+	public bool isHorizontal = false;   //横
 
 	void Start()
     {
-        
-    }
+		//parentObj = transform.parent.gameObject;
+		topParent_Script = topObj.GetComponent<Enemy_FollowGround_Ray>();
+		raytopParent_Script = parentObj.GetComponent<RayParent>();
+		angleChange_Script = angleObj.GetComponent<AngleChange>();
 
-    void Update()
+		myName = gameObject.name;
+
+		if (myName == "Col_Top" || myName == "Col_Under")
+		{
+			isVertical = true;
+		}
+		else
+		{
+			isHorizontal = true;
+		}
+	}
+
+	void Update()
     {
+		RayTest();
+		rayDelayCnt++;
 
-		// Bit shift the index of the layer (8) to get a bit mask
+	}
+
+	void RayTest()
+	{
 		int layerMask = 1 << 8;
 
-		// This would cast rays only against colliders in layer 8.
-		// But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
 		layerMask = ~layerMask;
 
-		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1.0f, layerMask))
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 0.1f, layerMask))
 		{
-
-			if (hit.collider.tag == "Player")
+			//rayDelayCnt++;
+			if (hit.collider.tag == "Wall")
 			{
-				groundAngle = Quaternion.FromToRotation(transform.forward, hit.normal).eulerAngles;
-				Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+				//if (rayDelayCnt > rayDelayMax)
+				if (angleChange_Script.delayCnt > angleChange_Script.delayMax)
+				{
+					groundAngle = Quaternion.FromToRotation(transform.forward, hit.normal).eulerAngles;
 
+					if (isVertical)
+					{
+						angleZ = groundAngle.z - 180.0f;
+
+					}
+					else if (isHorizontal)
+					{
+						angleZ = groundAngle.z - 270.0f;
+
+					}
+					//angleChange_Script.angleZ = angleZ;
+
+					if (angleChange_Script.angleZ != angleZ)
+					{
+						//angleChange_Script.angleZ = angleZ;
+						raytopParent_Script.angleZ = groundAngle.z - 180;
+						rayDelayCnt = 0;
+						angleChange_Script.delayCnt = 0;
+					}
+
+					//if (raytopParent_Script.angleZ != angleZ + 90)
+					//{
+					//	raytopParent_Script.angleZ = angleZ + 90;
+					//}
+
+					if (topParent_Script.angle != angleZ)
+					{
+						topParent_Script.angle = angleZ;
+					}
+				}
+				Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
 			}
 		}
 		else
 		{
-			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1.0F, Color.white);
+			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 0.1F, Color.white);
 		}
-
 	}
 }
---
+++ プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy2/RayParent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayParent : MonoBehaviour
+{
+	public float angleZ;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+		transform.rotation = Quaternion.Euler(0, 0, angleZ);
+	}
+}
