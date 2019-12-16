6faeb44785e00b59212e534cfc9abe0f18baf180
741PC-15\student <cu.common@gmail.com>
2019-12-16 16:18:37 +0900

    save

---
+++ プロトタイプ/ShootingGame/Assets/Script/kawamura/Enemy2/AngleChange2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleChange2 : MonoBehaviour
+{
+	public GameObject parentObj;
+
+	public FollowGround3 followGround_Script;
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.rotation = Quaternion.Euler(0, 0, -followGround_Script.groundAngle);
+    }
+}
