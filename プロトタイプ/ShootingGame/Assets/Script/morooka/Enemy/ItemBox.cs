5e056160ba902815e9d092e8a5c1a21745a2d79e
741PC-17\student <uroncha.15.3@gmail.com>
2020-01-09 16:27:52 +0900

    アイテムボックス作成

---
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/Enemy/ItemBox.cs
@@ -0,0 +1,27 @@
+﻿//----------------------------------------------------------------------------------------------
+// 制作者：諸岡勇樹
+// 制作日：2020/01/09
+//----------------------------------------------------------------------------------------------
+// アイテムボックス
+//----------------------------------------------------------------------------------------------
+//　2020/01/09　破壊時アイテム生成
+//----------------------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StorageReference;
+
+public class ItemBox : character_status
+{
+    new void Update()
+    {
+        if(hp < 1)
+		{
+			Died_Process();
+			Object_Instantiation.Object_Reboot(Game_Master.OBJECT_NAME.ePOWERUP_ITEM, transform.position, Quaternion.identity);
+		}
+
+		base.Update();
+	}
+}
