ab830974c0fb987a175db50db1ca486f47d6284f
741PC-17\student <uroncha.15.3@gmail.com>
2020-01-17 13:48:28 +0900

    触手入れた

--- プロトタイプ/ShootingGame/Assets/Script/morooka/BossScrits/Brain_Golem/Brain_Parts.cs
+++ プロトタイプ/ShootingGame/Assets/Script/morooka/BossScrits/Brain_Golem/Brain_Parts.cs
@@ -4,10 +4,51 @@ using UnityEngine;
 
 public class Brain_Parts : character_status
 {
+	enum Action
+	{
+		eANIM1,				//	1状態に移行アニメ
+		eANIM2,				//	2状態に移行アニメ
+		eANIM1_STAY,		//	1状態待機アニメ
+		eANIM2_STAY,		// 2状態待機アニメ
+	}
+
+	private Animation _Animation { get; set; }
+	private List<string> AnimName { get; set; }
+	private float Timer { get; set; }
+
+	private void Start()
+	{
+		AnimName = new List<string>(){"Anim1","Anim2","Anim1_Stay","Anim2_Stay"};
+		_Animation = GetComponent<Animation>();
+	}
+
 	private void Update()
 	{
+		Timer += Time.deltaTime;
+		if(_Animation.IsPlaying(AnimName[(int)Action.eANIM1]))
+		{
+			_Animation.CrossFade(AnimName[(int)Action.eANIM1_STAY]);
+		}
+		else if (_Animation.IsPlaying(AnimName[(int)Action.eANIM2]))
+		{
+			_Animation.CrossFade(AnimName[(int)Action.eANIM2_STAY]);
+		}
+
+		if(Timer > 6.0f)
+		{
+			if(_Animation.IsPlaying(AnimName[(int)Action.eANIM1_STAY]))
+			{
+				_Animation.CrossFade(AnimName[(int)Action.eANIM2]);
+			}
+			else if(_Animation.IsPlaying(AnimName[(int)Action.eANIM2_STAY]))
+			{
+				_Animation.CrossFade(AnimName[(int)Action.eANIM1]);
+			}
+			Timer = 0.0f;
+		}
+
 		// HPが0のとき
-		if(hp < 1)
+		if (hp < 1)
 		{
 			Died_Judgment();
 			enabled = false;
