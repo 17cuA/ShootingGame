8c30ae6aa0996253d3c111d322d6d9332cdda901
741PC-15\student <cu.common@gmail.com>
2020-01-27 15:42:53 +0900

    タイムラインでスクリプト呼び出す

---
+++ プロトタイプ/ShootingGame/Assets/Script/kawamura/Other/testcreateline.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testcreateline : MonoBehaviour
+{
+	public GameObject obj;
+	
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void Createeeeeeeeeeeeeeeee()
+	{
+		Instantiate(obj, transform.position, transform.rotation);
+	}
+}
