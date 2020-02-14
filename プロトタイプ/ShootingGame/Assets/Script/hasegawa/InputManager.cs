7ed0ea15c0254683be85fbcf3fbb284c9eb9550c
741PC-36\student <cu.common@gmail.com>
2020-02-14 15:44:03 +0900

    調整

--- プロトタイプ/ShootingGame/Assets/Script/hasegawa/InputManager.cs
+++ プロトタイプ/ShootingGame/Assets/Script/hasegawa/InputManager.cs
@@ -66,10 +66,10 @@ public class InputManager
 			Canvas anyCanvas = GameObject.FindObjectOfType<Canvas>();
 			inputInfoText = new GameObject("InputInfo").AddComponent<Text>();
 			inputInfoText.rectTransform.SetParent(anyCanvas.transform);
-			inputInfoText.rectTransform.localPosition = new Vector2(textPositionX / (3840f /Screen.width) , 0f);
+			inputInfoText.rectTransform.localPosition = new Vector2(textPositionX, 0f);
 			inputInfoText.font = textFont;
-			inputInfoText.fontSize = 50;
-			inputInfoText.rectTransform.sizeDelta = new Vector2(Screen.width, 1080f);
+			inputInfoText.fontSize = 50 / (int)(3840f / Screen.width);
+			inputInfoText.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
 		}
 		bool isComplete = false;
 		bool isInput = false;
