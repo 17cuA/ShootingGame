f3a349f1f8506869e36d167501980a201b36b12e
chenkenZzz <d512668247@gmail.com>
2020-02-27 01:03:29 +0900

    サンドバッグ　α完成　調整必要あり

--- プロトタイプ/ShootingGame/Assets/Script/Chenken/Trap/Trap.cs
+++ プロトタイプ/ShootingGame/Assets/Script/Chenken/Trap/Trap.cs
@@ -4,113 +4,95 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
-	[System.Serializable]
-	public struct HitAngleRange
-	{
-		public float min;
-		public float max;
-	}
+    public Transform midPoint;
+    public Transform movePoint;
+    [Range(9.8f,90f)] public float gravity = 9.8f;
+    [Range(1,10f)]public float friction = 0.05f;
+    [Range(0.1f,0.5f)]public float theltaLimit = 0.5f;
+    [Range(2f,25f)]public float force = 5;
+    [Range(0.5f,2f)] public float invaildTime = 0.25f;
+    private bool canHit = true;
+    private float invaildTimer;
+    private Vector3 rotateAxis;
+    private float w;
 
-	[Header("仕掛け設定、該当項目を選んでね★")]
-	public bool isTop = false;
-	public bool isBottom = false;
+    public bool isStepOne = false;
+    public bool isStepTwo = false;
+    public bool isHit = false;
 
-	[Header("移動範囲(角度)")]
-	public HitAngleRange hitAngleRange;
+    private void Start()
+    {
+        rotateAxis = Vector3.Cross (movePoint.position - midPoint.position, Vector3.down);
+    }
 
-	[Range(2,4)] public int hitCountMax;
-	private int hitCounter = 0;
+    private void Update()
+    {
+      
+        if (Input.GetMouseButtonDown(0) && canHit)
+        {
+            isHit = true;
+            canHit = false;
+        }
 
-	public float decountTime = 0.5f;
-	private float decountTimer;
+        if (!canHit)
+        {
+            invaildTimer += Time.deltaTime;
+            if(invaildTimer >= invaildTime)
+            {
+                invaildTimer = 0f;
+                canHit = true;
+            }
+        }
 
-	private float targetAngle;
-	private bool isInvaild = false;
-	[Range(0.05f,0.95f)] public float invaildTime = 0.5f;
-	private float invaildTimer;
+        if(isHit)
+        {
+            float r = Vector3.Distance(midPoint.position,movePoint.position);
+            w = force / r;
+            isStepOne = true;
+            isStepTwo = false;
+            isHit = false;
+           
+        }
 
-	public float returnAngle;
-	public float returnTime = 0.5f;
-	public int returnCountMax = 4;
-	private int returnCount;
-	private float returnTimer;
+        if(isStepOne)
+        {
+             float r = Vector3.Distance(midPoint.position,movePoint.position);
+            w -= (gravity - friction) / r * Time.deltaTime;
+            float thelta = w * Time.deltaTime * 180f / Mathf.PI;
+            transform.RotateAround(midPoint.position,Vector3.forward,thelta);
+            if(w <= 0.1f)
+            {
+                 rotateAxis = Vector3.Cross (movePoint.position - midPoint.position, Vector3.down);
+                isStepTwo = true;
+                isStepOne = false;
+            }
+        }
+        if (isStepTwo)
+        {
+             float r = Vector3.Distance(midPoint.position,movePoint.position);
+             float l = Vector3.Distance(new Vector3(midPoint.position.x,movePoint.position.y,midPoint.position.z),movePoint.position);
+             Vector3 axis = Vector3.Cross(movePoint.position - midPoint.position,Vector3.down);
+             if(Vector3.Dot(axis,rotateAxis) < 0)
+             {
+                 l *= -1;
+             }
+             float cosAlpha = l / r;     //cos値
+             float alpha =  (cosAlpha * gravity - friction) / r;
+             w += alpha * Time.deltaTime;
+             float thelta = w * Time.deltaTime * 180f / Mathf.PI;
+             transform.RotateAround(midPoint.position,rotateAxis,thelta);
+             if(Mathf.Abs(thelta) <= theltaLimit)
+             {
+                 rotateAxis = Vector3.Cross (movePoint.position - midPoint.position, Vector3.down);
+                 if(Mathf.Abs(l) <= 0.5f)
+                 {
+                     rotateAxis = Vector3.zero;
+                     isStepTwo = false;
+                     w = 0;
+                     Debug.Log("Over");
+                 }
+             }
+        }
 
-	private bool isToTarget = false;
-	private bool isReturning = false;
-
-	private void Update()
-	{
-		if(Input.GetMouseButtonDown(0))
-		{
-			if (hitCounter < hitCountMax && !isInvaild)
-			{
-				hitCounter++;
-
-				targetAngle = Mathf.Lerp(hitAngleRange.min, hitAngleRange.max, (float)(hitCounter) / (float)(hitCountMax));
-				isInvaild = true;
-				isToTarget = true;
-			}
-		}
-
-		if(isInvaild)
-		{
-			invaildTimer += Time.deltaTime;
-			if(invaildTimer >= invaildTime)
-			{
-				isInvaild = false;
-				invaildTime = 0f;
-			}
-		}
-
-		if(hitCounter > 0)
-		{
-			decountTimer += Time.deltaTime;
-			if(decountTimer > decountTime)
-			{
-				hitCounter--;
-				decountTimer = 0;
-			}
-		}
-
-		if(isToTarget)
-		{
-			var x = Mathf.Lerp(transform.localEulerAngles.x, targetAngle, returnTimer / returnTime);
-			transform.localEulerAngles = new Vector3(x, 90, 90);
-
-			returnTimer += Time.deltaTime;
-
-			if(returnTimer >= returnTime)
-			{
-				isToTarget = false;
-				isReturning = true;
-				returnTimer = 0;
-			}
-		}
-		else
-		{
-			if(isReturning)
-			{
-				var x = Mathf.Lerp(transform.localEulerAngles.x, returnAngle, returnTimer / returnTime);
-				transform.localEulerAngles = new Vector3(x, 90, 90);
-
-				returnTimer += Time.deltaTime;
-
-				if (returnTimer >= returnTime)
-				{
-					returnCount++;
-
-					var delta = Mathf.Abs(Mathf.Abs(transform.localEulerAngles.x) - 90);
-					var randomAngle = UnityEngine.Random.Range(delta / 2, delta);
-					returnAngle = -90 + randomAngle * (-1) * returnCount;
-
-					returnTimer = 0;
-					if (returnCount == returnCountMax)
-					{
-						isReturning = false;
-						return;
-					}
-				}
-			}
-		}
-	}
+    }
 }
