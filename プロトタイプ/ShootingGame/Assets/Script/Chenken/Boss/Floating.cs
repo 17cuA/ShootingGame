dc47d53acc6f2007b86c7e25e4891799217a4c75
chenkenZzz <d512668247@gmail.com>
2020-03-04 11:42:33 +0900

    修正

---
+++ プロトタイプ/ShootingGame/Assets/Script/Chenken/Boss/Floating.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+public class Floating : MonoBehaviour
+{
+	private float radian = 0;
+	[Range(0.01f,0.03f)] public float perRadian = 0.03f;
+	[Range(0.3f,1f)]     public float radius = 0.8f;
+	private Vector3 oldPos; 
+	
+
+	private void Start()
+	{
+		oldPos = transform.position; 
+	}
+
+	private void Update()
+	{
+		radian += perRadian;
+		float dy = Mathf.Cos(radian) * radius; 
+		transform.position = oldPos + new Vector3(0, dy, 0);
+	}
+}
