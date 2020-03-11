cade8d958972d4c1ba866f4ba36093f9c829d874
741PC-34\student <cu.common@gmail.com>
2020-03-11 13:21:35 +0900

    ED背景とクソダサシリーズ追加

---
+++ プロトタイプ/ShootingGame/Assets/Effects/Atelier_Takada/Effect/_KUSODASA/KUSODASATITLE.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KUSODASATITLE : MonoBehaviour
+{
+	float time = 0f;
+	float sceneChangeTime = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		time = 0f;
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		time += Time.deltaTime;
+		if (time > sceneChangeTime)
+		{
+			SceneManager.LoadScene("KUSODASATITLE");
+		}
+
+
+    }
+}
